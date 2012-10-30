fdf0d9e75b2d4e78cce12edf44cfee4962bfa739
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2012-10-30 14:38:25 +0000

    added event description class
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@19418 4525493e-7705-40b1-a816-d608a930855b

---
+++ SusyAnalysisTools/include/EventDescription.h
@@ -0,0 +1,149 @@
+// Dear emacs, this is -*- c++ -*-
+#ifndef SusyAnalysisTools_EventDescription_h
+#define SusyAnalysisTools_EventDescription_h
+
+// ============================================================================
+namespace SusyAnalysisTools
+{
+  enum FLAVOR_CHANNEL { FLAVOR_NONE = 0
+                      , FLAVOR_EE   = 1
+                      , FLAVOR_MM   = 2
+                      , FLAVOR_EM   = 4
+                      };
+  enum TRIGGER_CHANNEL { TRIGGER_NONE = 0
+                       , TRIGGER_EE   = 1
+                       , TRIGGER_MM   = 2
+                       , TRIGGER_EM   = 4
+                       };
+  enum PHASE_SPACE { PHASE_NONE = 0
+                   , PHASE_EE   = 1
+                   , PHASE_MM   = 2
+                   , PHASE_EM   = 4
+                   , PHASE_ME   = 8
+                   };
+  enum SIGN_CHANNEL { SIGN_NONE = 0
+                    , SIGN_OS   = 1
+                    , SIGN_SS   = 2
+                    };
+
+  // ==========================================================================
+  class EventDescription
+  {
+    // ------------------------------------------------------------------------
+    public:
+      EventDescription();
+      ~EventDescription();
+
+      unsigned long long toInt() const;
+
+      EventDescription& operator=(const EventDescription&);
+      EventDescription& operator=(const unsigned long long&);
+
+      void setPassGrl(bool);
+      void setPassIncompleteEvent(bool);
+      void setPassLarError(bool);
+      void setPassTileCalHotSpot(bool);
+      void setPassBadJets(bool);
+      void setPassPrimaryVertex(bool);
+      void setPassBadMuons(bool);
+      void setPassCosmicMuons(bool);
+      void setPassHFOR(bool);
+      void setPassGE2GoodLeptons(bool);
+      void setPass2GoodLeptons(bool);
+      void setPassMll(bool);
+      void setPass2SignalLeptons(bool);
+      void setPassTriggerMatch(bool);
+      void setFlavorChannel(FLAVOR_CHANNEL);
+      void setPhaseSpace(PHASE_SPACE);
+      void setTriggerChannel(TRIGGER_CHANNEL);
+      void setSignChannel(SIGN_CHANNEL);
+
+      void setTruthMatched(bool);
+      void setTruthPrompt(bool);
+      void setCFCandidate(bool);
+      void setTruthSignChannel(SIGN_CHANNEL);
+
+      bool getPassGrl();
+      bool getPassIncompleteEVent();
+      bool getPassLarError();
+      bool getPassTileCalHotSpot();
+      bool getPassBadJets();
+      bool getPassPrimaryVertex();
+      bool getPassBadMuons();
+      bool getPassCosmicMuons();
+      bool getPassHFOR();
+      bool getPassGE2GoodLeptons();
+      bool getPass2GoodLeptons();
+      bool getPassMll();
+      bool getPass2SignalLeptons();
+      bool getPassTriggerMatch();
+      FLAVOR_CHANNEL  getFlavorChannel();
+      PHASE_SPACE     getPhaseSpace();
+      TRIGGER_CHANNEL getTriggerChannel();
+      SIGN_CHANNEL    getSignChannel();
+
+      bool getTruthMatched();
+      bool getTruthPrompt();
+      bool getCFCandidate();
+      SIGN_CHANNEL getTruthSignChannel();
+
+    // ------------------------------------------------------------------------
+    private:
+      static const unsigned long long SIZE_BOOL    = 1;
+      static const unsigned long long SIZE_FLAVOR  = 3;
+      static const unsigned long long SIZE_TRIGGER = 3;
+      static const unsigned long long SIZE_PHASE   = 4;
+      static const unsigned long long SIZE_SIGN    = 2;
+
+      static const unsigned long long ADD_GRL                = 0;
+      static const unsigned long long ADD_INCOMPLETE_EVENT   = 1;
+      static const unsigned long long ADD_LAR_ERROR          = 2;
+      static const unsigned long long ADD_TILE_CAL_HOT_SPOT  = 3;
+      static const unsigned long long ADD_BAD_JETS           = 4;
+      static const unsigned long long ADD_PRIMARY_VERTEX     = 5;
+      static const unsigned long long ADD_BAD_MUONS          = 6;
+      static const unsigned long long ADD_COSMIC_MUONS       = 7;
+      static const unsigned long long ADD_HFOR               = 8;
+      static const unsigned long long ADD_GE_2_GOOD_LEPTONS  = 9;
+      static const unsigned long long ADD_2_GOOD_LEPTONS     = 10;
+      static const unsigned long long ADD_MLL                = 11;
+      static const unsigned long long ADD_2_SIGNAL_LEPTONS   = 12;
+      static const unsigned long long ADD_FLAVOR_CHANNEL     = 13;
+      static const unsigned long long ADD_PHASE_SPACE        = 16;
+      static const unsigned long long ADD_TRIGGER_CHANNEL    = 20;
+      static const unsigned long long ADD_TRIGGER_MATCH      = 23;
+      static const unsigned long long ADD_SIGN_CHANNEL       = 24;
+      static const unsigned long long ADD_TRUTH_SIGN_CHANNEL = 30;
+      static const unsigned long long ADD_TRUTH_MATCHED      = 32;
+      static const unsigned long long ADD_TRUTH_PROMPT       = 33;
+      static const unsigned long long ADD_TRUTH_CF_CANDIDATE = 34;
+
+      bool            m_pass_grl;
+      bool            m_pass_incomplete_event;
+      bool            m_pass_lar_error;
+      bool            m_pass_tile_cal_hot_spot;
+      bool            m_pass_bad_jets;
+      bool            m_pass_primary_vertex;
+      bool            m_pass_bad_muons;
+      bool            m_pass_cosmic_muons;
+      bool            m_pass_hfor;
+      bool            m_pass_ge_2_good_leptons;
+      bool            m_pass_2_good_leptons;
+      bool            m_pass_mll;
+      bool            m_pass_2_signal_leptons;
+      FLAVOR_CHANNEL  m_flavor_channel;
+      PHASE_SPACE     m_phase_space;
+      TRIGGER_CHANNEL m_trigger_channel;
+      bool            m_pass_trigger_match;
+      SIGN_CHANNEL    m_sign_channel;
+
+      SIGN_CHANNEL m_truth_sign_channel;
+      bool         m_truth_matched;
+      bool         m_truth_prompt;
+      bool         m_cf_candidate;
+  };
+
+} /* end of SusyAnalysisTools */
+
+
+#endif /* end of include guard: SusyAnalysisTools_EventDescription_h */
---
+++ SusyAnalysisTools/src/EventDescription.cxx
@@ -0,0 +1,376 @@
+#include "include/EventDescription.h"
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::EventDescription::EventDescription()
+{
+  // do nothing
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::EventDescription::~EventDescription()
+{
+  // do nothing
+}
+
+// ----------------------------------------------------------------------------
+unsigned long long SusyAnalysisTools::EventDescription::toInt() const
+{
+  unsigned long long event_desc = 0;
+
+  event_desc += (  static_cast<unsigned long long>(m_pass_grl)
+                << ADD_GRL);
+  event_desc += (  static_cast<unsigned long long>(m_pass_incomplete_event)
+                << ADD_INCOMPLETE_EVENT);
+  event_desc += (  static_cast<unsigned long long>(m_pass_lar_error)
+                << ADD_LAR_ERROR);
+  event_desc += (  static_cast<unsigned long long>(m_pass_tile_cal_hot_spot)
+                << ADD_TILE_CAL_HOT_SPOT);
+  event_desc += (  static_cast<unsigned long long>(m_pass_bad_jets)
+                << ADD_BAD_JETS);
+  event_desc += (  static_cast<unsigned long long>(m_pass_primary_vertex)
+                << ADD_PRIMARY_VERTEX);
+  event_desc += (  static_cast<unsigned long long>(m_pass_bad_muons)
+                << ADD_BAD_MUONS);
+  event_desc += (  static_cast<unsigned long long>(m_pass_cosmic_muons)
+                << ADD_COSMIC_MUONS);
+  event_desc += (  static_cast<unsigned long long>(m_pass_hfor)
+                << ADD_HFOR);
+  event_desc += (static_cast<unsigned long long>(m_pass_ge_2_good_leptons)
+                << ADD_GE_2_GOOD_LEPTONS);
+  event_desc += (  static_cast<unsigned long long>(m_pass_2_good_leptons)
+                << ADD_2_GOOD_LEPTONS);
+  event_desc += (  static_cast<unsigned long long>(m_pass_mll)
+                << ADD_MLL);
+  event_desc += (  static_cast<unsigned long long>(m_pass_2_signal_leptons)
+                << ADD_2_SIGNAL_LEPTONS);
+  event_desc += (  static_cast<unsigned long long>(m_flavor_channel)
+                << ADD_FLAVOR_CHANNEL);
+  event_desc += (  static_cast<unsigned long long>(m_phase_space)
+                << ADD_PHASE_SPACE);
+  event_desc += (  static_cast<unsigned long long>(m_trigger_channel)
+                << ADD_TRIGGER_CHANNEL);
+  event_desc += (  static_cast<unsigned long long>(m_pass_trigger_match)
+                << ADD_TRIGGER_MATCH);
+  event_desc += (  static_cast<unsigned long long>(m_sign_channel)
+                << ADD_SIGN_CHANNEL);
+  event_desc += (  static_cast<unsigned long long>(m_truth_sign_channel)
+                << ADD_TRUTH_SIGN_CHANNEL);
+  event_desc += (  static_cast<unsigned long long>(m_truth_matched)
+                << ADD_TRUTH_MATCHED);
+  event_desc += (  static_cast<unsigned long long>(m_truth_prompt)
+                << ADD_TRUTH_PROMPT);
+  event_desc += (  static_cast<unsigned long long>(m_cf_candidate)
+                << ADD_TRUTH_CF_CANDIDATE);
+
+  return event_desc;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::EventDescription&
+    SusyAnalysisTools::EventDescription::operator=(
+        const SusyAnalysisTools::EventDescription& rhs)
+{
+  *this = rhs.toInt();
+  return *this;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::EventDescription&
+    SusyAnalysisTools::EventDescription::operator=(
+        const unsigned long long& rhs)
+{
+  m_pass_grl               = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_GRL               ) );
+  m_pass_incomplete_event  = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_INCOMPLETE_EVENT  ) );
+  m_pass_lar_error         = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_LAR_ERROR         ) );
+  m_pass_tile_cal_hot_spot = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_TILE_CAL_HOT_SPOT ) );
+  m_pass_bad_jets          = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_BAD_JETS          ) );
+  m_pass_primary_vertex    = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_PRIMARY_VERTEX    ) );
+  m_pass_bad_muons         = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_BAD_MUONS         ) );
+  m_pass_cosmic_muons      = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_COSMIC_MUONS      ) );
+  m_pass_hfor              = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_HFOR              ) );
+  m_pass_ge_2_good_leptons = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_GE_2_GOOD_LEPTONS ) );
+  m_pass_2_good_leptons    = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_2_GOOD_LEPTONS    ) );
+  m_pass_mll               = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_MLL               ) );
+  m_pass_2_signal_leptons  = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_2_SIGNAL_LEPTONS  ) );
+  m_pass_trigger_match     = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_TRIGGER_MATCH     ) );
+  m_truth_matched          = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_TRUTH_MATCHED     ) );
+  m_truth_prompt           = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_TRUTH_PROMPT      ) );
+  m_cf_candidate           = ( ((1 << SIZE_BOOL)-1 ) & (rhs >> ADD_TRUTH_CF_CANDIDATE) );
+
+  m_flavor_channel =
+    static_cast<FLAVOR_CHANNEL>( ((1 << SIZE_FLAVOR )-1) & (rhs >> ADD_FLAVOR_CHANNEL) );
+  m_phase_space =
+    static_cast<PHASE_SPACE>( ((1 << SIZE_PHASE)-1) & (rhs >> ADD_PHASE_SPACE) );
+  m_trigger_channel =
+    static_cast<TRIGGER_CHANNEL>( ((1 << SIZE_TRIGGER)-1) & (rhs >> ADD_TRIGGER_CHANNEL) );
+  m_sign_channel =
+    static_cast<SIGN_CHANNEL>( ((1 << SIZE_SIGN)-1) & (rhs >> ADD_SIGN_CHANNEL) );
+  m_truth_sign_channel =
+    static_cast<SIGN_CHANNEL>( ((1 << SIZE_SIGN)-1) & (rhs >> ADD_TRUTH_SIGN_CHANNEL) );
+
+  return *this;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassGrl(bool pass_grl)
+{
+  m_pass_grl = pass_grl;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassIncompleteEvent(bool pass_incomplete_event)
+{
+  m_pass_incomplete_event = pass_incomplete_event;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassLarError(bool pass_lar_error)
+{
+  m_pass_lar_error = pass_lar_error;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassTileCalHotSpot(bool pass_tile_cal_hot_spot)
+{
+  m_pass_tile_cal_hot_spot = pass_tile_cal_hot_spot;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassBadJets(bool pass_bad_jets)
+{
+  m_pass_bad_jets = pass_bad_jets;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassPrimaryVertex(bool pass_primary_vertex)
+{
+  m_pass_primary_vertex = pass_primary_vertex;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassBadMuons(bool pass_bad_muons)
+{
+  m_pass_bad_muons = pass_bad_muons;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassCosmicMuons(bool pass_cosmic_muons)
+{
+  m_pass_cosmic_muons = pass_cosmic_muons;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassHFOR(bool pass_hfor)
+{
+  m_pass_hfor = pass_hfor;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassGE2GoodLeptons(bool pass_ge_2_good_leptons)
+{
+  m_pass_ge_2_good_leptons = pass_ge_2_good_leptons;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPass2GoodLeptons(bool pass_2_good_leptons)
+{
+  m_pass_2_good_leptons = pass_2_good_leptons;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassMll(bool pass_mll)
+{
+  m_pass_mll = pass_mll;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPass2SignalLeptons(bool pass_2_signal_leptons)
+{
+  m_pass_2_signal_leptons = pass_2_signal_leptons;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPassTriggerMatch(bool pass_trigger_match)
+{
+  m_pass_trigger_match = pass_trigger_match;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setFlavorChannel(SusyAnalysisTools::FLAVOR_CHANNEL flavor_channel)
+{
+  m_flavor_channel = flavor_channel;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setPhaseSpace(SusyAnalysisTools::PHASE_SPACE phase_space)
+{
+  m_phase_space = phase_space;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setTriggerChannel(SusyAnalysisTools::TRIGGER_CHANNEL trigger_channel)
+{
+  m_trigger_channel = trigger_channel;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setSignChannel(SusyAnalysisTools::SIGN_CHANNEL sign_channel)
+{
+  m_sign_channel = sign_channel;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setTruthMatched(bool truth_matched)
+{
+  m_truth_matched = truth_matched;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setTruthPrompt(bool truth_prompt)
+{
+  m_truth_prompt = truth_prompt;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setCFCandidate(bool cf_candidate)
+{
+  m_cf_candidate = cf_candidate;
+}
+
+// ----------------------------------------------------------------------------
+void SusyAnalysisTools::EventDescription::setTruthSignChannel(SusyAnalysisTools::SIGN_CHANNEL truth_sign_channel)
+{
+  m_truth_sign_channel = truth_sign_channel;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassGrl()
+{
+  return m_pass_grl;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassIncompleteEVent()
+{
+  return m_pass_incomplete_event;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassLarError()
+{
+  return m_pass_lar_error;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassTileCalHotSpot()
+{
+  return m_pass_tile_cal_hot_spot;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassBadJets()
+{
+  return m_pass_bad_jets;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassPrimaryVertex()
+{
+  return m_pass_primary_vertex;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassBadMuons()
+{
+  return m_pass_bad_muons;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassCosmicMuons()
+{
+  return m_pass_cosmic_muons;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassHFOR()
+{
+  return m_pass_hfor;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassGE2GoodLeptons()
+{
+  return m_pass_ge_2_good_leptons;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPass2GoodLeptons()
+{
+  return m_pass_2_good_leptons;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassMll()
+{
+  return m_pass_mll;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPass2SignalLeptons()
+{
+  return m_pass_2_signal_leptons;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getPassTriggerMatch()
+{
+  return m_pass_trigger_match;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::FLAVOR_CHANNEL SusyAnalysisTools::EventDescription::getFlavorChannel()
+{
+  return m_flavor_channel;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::PHASE_SPACE SusyAnalysisTools::EventDescription::getPhaseSpace()
+{
+  return m_phase_space;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::TRIGGER_CHANNEL SusyAnalysisTools::EventDescription::getTriggerChannel()
+{
+  return m_trigger_channel;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::SIGN_CHANNEL SusyAnalysisTools::EventDescription::getSignChannel()
+{
+  return m_sign_channel;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getTruthMatched()
+{
+  return m_truth_matched;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getTruthPrompt()
+{
+  return m_truth_prompt;
+}
+
+// ----------------------------------------------------------------------------
+bool SusyAnalysisTools::EventDescription::getCFCandidate()
+{
+  return m_cf_candidate;
+}
+
+// ----------------------------------------------------------------------------
+SusyAnalysisTools::SIGN_CHANNEL SusyAnalysisTools::EventDescription::getTruthSignChannel()
+{
+  return m_truth_sign_channel;
+}
