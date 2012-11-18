b0ed526446fbc0bfdb158db22f77433a8ddeeafc
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2012-11-18 22:15:46 +0000

    updata run scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@19906 4525493e-7705-40b1-a816-d608a930855b

---
+++ scripts/run_scripts/gen_susy_dilepton_config.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+"""
+write a docstring and add an example usage
+"""
+
+import sys
+import os.path
+import subprocess
+import optparse
+import time
+
+import SFrameHelpers
+
+import parse_yaml_config as parse
+
+# ------------------------------------------------------------------------------
+def main():
+    config_dict = parse.parseInputs()
+    writeConfigXml(config_dict, 'test_file.xml')
+
+# ------------------------------------------------------------------------------
+def getJobLabel(inputs):
+    label = inputs.base_label
+    return label
+
+# ------------------------------------------------------------------------------
+def writeFileLines(out_file, file_name_list):
+    for fn in file_name_list:
+        out_file.write('      <In FileName="%s" Lumi="1.0" />\n' % fn)
+
+# ------------------------------------------------------------------------------
+def writeConfigLine(out_file, name, value):
+    line = '      <Item Name="%s" Value="%s" />\n' % (name, value)
+    out_file.write(line)
+
+# ------------------------------------------------------------------------------
+def writeUserConfigs(out_file, config_dict):
+    for cd in config_dict['UserConfig']:
+        if type(config_dict['UserConfig'][cd]) == dict:
+            for item in config_dict['UserConfig'][cd]:
+                name = '%s_%s' % (cd, item)
+                value = config_dict['UserConfig'][cd][item]
+                writeConfigLine(out_file, name, value)
+        else:
+            value = config_dict['UserConfig'][cd]
+            writeConfigLine(out_file, cd, value)
+
+# ------------------------------------------------------------------------------
+def writeConfigXml(config_dict, out_file):
+    f = open(out_file, 'w')
+
+    # get base path
+    base_path = os.environ['SFRAME_DIR']
+    base_path = base_path[0:base_path.find('/SFrame')]
+    base_path = '%s/' % base_path
+
+    # write header information
+    f.write( """<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<!DOCTYPE JobConfiguration PUBLIC "" "%s/SusyDiLepton/config/JobConfig.dtd">
+""" % base_path)
+
+    f.write( """<JobConfiguration JobName="%(JobName)s" OutputLevel="%(OutputLevel)s">
+""" % config_dict)
+
+    # write libraries that must be loaded for cycle to run 
+    f.write("""  <!-- List of libraries to be loaded for the analysis. -->
+  <Library Name="libGenVector" />
+  <Library Name="libGraf" />
+  <Library Name="libCintex" />
+  <Library Name="libSFrameCintex" />
+  <Library Name="libSFramePlugIns" />
+
+  <Library Name="libGoodRunsLists" />
+  <Library Name="libegammaAnalysisUtils" />
+  <Library Name="libJetResolution" />
+  <Library Name="libMissingETUtility" />
+  <Library Name="libMuonEfficiencyCorrections" />
+  <Library Name="libMuonMomentumCorrections" />
+  <Library Name="libPileupReweighting" />
+  <Library Name="libReweightUtils" />
+  <Library Name="libCalibrationDataInterface" />
+  <Library Name="libSUSYTools" />
+  <Library Name="libSusyMatrixMethod" />
+  <Library Name="libHistFitterTree" />
+  <Library Name="libChargeFlip" />
+  <Library Name="libLeptonTruthTools" />
+  <Library Name="libReweightUtils" />
+  <Library Name="libDGTriggerReweight" />
+  <Library Name="libTriggerMatch" />
+  <Library Name="libApplyJetCalibration" />
+  <Library Name="libD3PDObjects" />
+  <Library Name="libAtlasSFrameUtils" />
+  <Library Name="libCommonTools" />
+  <Library Name="libSelectionTools" />
+  <Library Name="libSusyAnalysisTools" />
+  <Library Name="libSusyDiLepton" />
+""")
+    f.write('\n')
+
+    # this cycle
+    f.write("""  <!-- List of cycles to be executed -->
+  <Cycle Name="%(CycleName)s" TargetLumi="-1"
+    OutputDirectory="%(OutputDirectory)s"
+    PostFix=".%(PostFix)s"
+    RunMode="LOCAL" >
+""" % config_dict)
+    f.write('\n')
+
+    # input file info
+    f.write("""  <!-- files to be inputed -->
+    <InputData Type="%(Type)s" Version="%(Version)s" Lumi="0"
+      NEventsMax="%(NEventsMax)s" Cacheable="False" SkipValid="False">
+""" % config_dict)
+    f.write('\n')
+
+    # actual files to include
+    f.write('      <!-- include input data collection -->\n')
+    writeFileLines(f, config_dict['InputFiles'])
+    f.write('\n')
+
+    # finish InputData block
+    f.write("""      <!-- Specify the input/output trees -->
+      <InputTree Name="susy" />
+      <OutputTree Name="output" />
+    </InputData>
+""")
+    f.write('\n')
+
+    # user configurables
+    f.write('    <!-- User configurables for this Cycle -->\n')
+    f.write('    <UserConfig>\n')
+    writeUserConfigs(f, config_dict)
+    f.write("    </UserConfig>\n")
+
+    # close remaining black
+    f.write("  </Cycle>\n")
+    f.write("</JobConfiguration>\n")
+
+
+# ==============================================================================
+if __name__ == '__main__':
+  sys.exit(main())
--- scripts/run_scripts/parse_yaml_config.py
+++ scripts/run_scripts/parse_yaml_config.py
@@ -6,58 +6,56 @@ import optparse
 import time
 
 import yaml
+import find_latest_grl as grl
+
+# ------------------------------------------------------------------------------
+def parseInputs():
+    global_config_file = sys.argv[1]
+    print global_config_file
+
+    config_dict = processConfigFile(global_config_file)
+
+    return config_dict
+
+# ------------------------------------------------------------------------------
+def processConfigFile(global_config_file_name):
+    print 'global_config_file_name: %s ' % global_config_file_name
+    global_config_file = open(global_config_file_name)
+    config_dict = yaml.load(global_config_file)
+
+    user_config_file_name = config_dict['UserConfigFile']
+    print 'user_config_file_name: %s' % user_config_file_name
+    user_config_file = open(user_config_file_name)
+    config_dict['UserConfig'] = yaml.load(user_config_file)
+
+    processConfigurables(config_dict)
+
+    return config_dict
 
 # ------------------------------------------------------------------------------
 def processConfigurables(config):
+    # get job name and Cycle name
+    config['JobName'] = config['CycleName'] = 'SusyDiLeptonCutFlowCycle'
+
+    # construct postfix using SampleName
+    config['PostFix'] = config['SampleName']
+
     # configure egamma energy rescale and jet calibration tools with is_af2
-    if config['EgammaEnergyRescale']['is_af2'] == 'CONFIG':
-        config['EgammaEnergyRescale']['is_af2'] = config['Global']['is_af2']
+    if config['UserConfig']['EgammaEnergyRescale']['is_af2'] == 'CONFIG':
+        config['UserConfig']['EgammaEnergyRescale']['is_af2'] = \
+            config['is_af2']
 
-    if config['JetCalibration']['is_af2'] == 'CONFIG':
-        config['JetCalibration']['is_af2'] = config['Global']['is_af2']
-
-    del config['Global']['is_af2']
+    if config['UserConfig']['JetCalibration']['is_af2'] == 'CONFIG':
+        config['UserConfig']['JetCalibration']['is_af2'] = \
+            config['is_af2']
 
     # if requested, find GRL
-    if config['GRL']['grl_xml'] == 'FIND_ME':
-        # TODO find correct GRL
-        pass
+    if config['UserConfig']['GRL']['do_grl'] \
+            and config['UserConfig']['GRL']['grl_xml'] == 'FIND_ME':
+        config['UserConfig']['GRL']['grl_xml'] = grl.find_latest_grl('All_Good')
+        print 'Setting GRL xml to "%s"\n' % config['UserConfig']['GRL']['grl_xml']
 
-# ------------------------------------------------------------------------------
-def printToXml(config, out_file_name):
-    out_file = open(out_file_name, "w")
-
-    out_file.write('<UserConfig>')
-    for key1 in config:
-        for key2 in config[key1]:
-            name = '%s_%s' % (key1, key2)
-            if key1 == "Global":
-                name = key2
-            value = config[key1][key2]
-            out_file.write(formXmlItemLine(name, value))
-    out_file.write('</UserConfig>')
-    out_file.close()
-            
-# ------------------------------------------------------------------------------
-def formXmlItemLine(name, value):
-    line = '<Item Name="%s" Value="%s" />\n' % (name, value)
-    return line
-
-# ------------------------------------------------------------------------------
-def parseYamlInput(in_file_name, out_file_name):
-    config_file = open(in_file_name)
-    config = yaml.load(config_file)
-
-    printToXml(config, out_file_name)
-
-# ------------------------------------------------------------------------------
-def main():
-    config_file_name = 'job_options.yml'
-    config_file = open(config_file_name)
-    config = yaml.load(config_file)
-
-    printToXml(config, 'job_options.xml')
-
-# ==============================================================================
-if __name__ == '__main__':
-    main()
+    config['UserConfig']['super_verbose_info'] = config['SuperVerboseInfo']
+    config['UserConfig']['print_event_info']   = config['PrintEventInfo']
+    config['UserConfig']['input_tree_name'] = config['input_tree_name']
+    config['UserConfig']['output_tree_name'] = config['output_tree_name']
