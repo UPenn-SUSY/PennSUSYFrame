b6700d8b63602005af5cb4532f3a75e84c446049
ehines <ehines@4525493e-7705-40b1-a816-d608a930855b>
2012-11-23 15:21:28 +0000

    HFORTool
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@20022 4525493e-7705-40b1-a816-d608a930855b

---
+++ SelectionTools/include/HFORTool.h
@@ -0,0 +1,44 @@
+// Dear emacs, this is -*- c++ -*-
+#ifndef SelectionTools_HFORTool_h
+#define SelectionTools_HFORTool_h
+
+
+#include "AtlasSFrameUtils/include/ToolBase.h"
+
+#include "D3PDObjects/include/TruthD3PDObject.h"
+
+#include "SUSYTools/HforToolD3PD.h"
+
+
+// =============================================================================
+namespace SelectionTools
+{
+  // ===========================================================================
+  class HFORTool : public ToolBase
+  {
+    // ---------------------------------------------------------------------------
+  public:
+    
+    HFORTool( SCycleBase* parent
+            , const char* name = "EventCleaning"
+	    );
+
+    virtual ~HFORTool();
+
+    bool passHFOR(const D3PDReader::TruthD3PDObject*);
+
+
+  // ---------------------------------------------------------------------------
+  private:
+
+    bool c_do_hfor;
+
+    HforToolD3PD m_hforTool;
+
+    ClassDef(HFORTool, 0);
+
+  };
+
+}
+
+#endif
---
+++ SelectionTools/src/HFORTool.cxx
@@ -0,0 +1,28 @@
+#include "include/HFORTool.h"
+
+// -----------------------------------------------------------------------------
+SelectionTools::HFORTool::HFORTool(
+    SCycleBase* parent, const char* name): ToolBase(parent, name)
+{
+  DeclareProperty("do_hfor", c_do_hfor = true);
+}
+// -----------------------------------------------------------------------------
+SelectionTools::HFORTool::~HFORTool()
+{
+  //do nothing
+}
+// -----------------------------------------------------------------------------
+bool SelectionTools::HFORTool::passHFOR(const D3PDReader::TruthD3PDObject* mc)
+{
+  
+  if (!c_do_hfor) return true;
+  
+  int hfor_type = m_hforTool.getDecision(mc->mc_channel_number(), mc->mc_n(), mc->mc_pt(), mc->mc_eta(), mc->mc_phi(),mc->mc_m(), mc->mc_pdgId(), mc->mc_status(), mc->mc_vx_barcode(), mc->mc_parent_index(), mc->mc_child_index(), HforToolD3PD::DEFAULT);
+ 
+
+  if (hfor_type == 4) return false;
+
+  return true;
+
+ 
+}
