59879f479d54e55ac709f51040d852e49b3733c6
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2012-12-09 01:23:11 +0000

    update utils
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@20387 4525493e-7705-40b1-a816-d608a930855b

---
+++ utils/Plotting/HistHandle/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#------------------------------------------------------------------------------
+# module metadata
+#------------------------------------------------------------------------------
+__author__ = 'Brett Jackson'
+__email__ = 'brett.david.jackson@cern.ch'
+__copyright__ = 'Copyright 2008-2012 Ryan D. Reece'
+__license__ = 'GPL http://www.gnu.org/licenses/gpl.html'
+
+default = object()
+ignore_zeros = object()
+
+left = object()
+right = object()
+
+import Handle, Merger, Objects, Painter, parse, Helper
