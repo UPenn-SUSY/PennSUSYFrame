45b24c5ddfa327ba3cd5f33a9f9ca7f876f3de44
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-01-25 10:44:39 +0000

    update hists to be plotted in optimization
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@21471 4525493e-7705-40b1-a816-d608a930855b

--- utils/HistMaker/run/OptimizationHistograms/gen_cut_config.py
+++ utils/HistMaker/run/OptimizationHistograms/gen_cut_config.py
@@ -12,73 +12,90 @@ def main():
         return
 
     file_base = sys.argv[1]
-    template_file_name   = '%s.template.config' % file_base
-    base_cut_file_name   = '%s.config'          % file_base
-    fake_cut_file_name   = '%s.fake.config'   % file_base
-    signal_cut_file_name = '%s.signal.config'     % file_base
+    template_file_name    = '%s.template.config' % file_base
+    nominal_cut_file_name = '%s.nominal.config'  % file_base
+    fake_cut_file_name    = '%s.fake.config'     % file_base
+    signal_cut_file_name  = '%s.signal.config'   % file_base
+    cf_cut_file_name      = '%s.cf.config'       % file_base
     print template_file_name
-    print base_cut_file_name
+    print nominal_cut_file_name
     print fake_cut_file_name
     print signal_cut_file_name
+    print cf_cut_file_name
 
-    template_file   = open(template_file_name  , 'r')
-    base_cut_file   = open(base_cut_file_name  , 'w')
-    fake_cut_file   = open(fake_cut_file_name  , 'w')
-    signal_cut_file = open(signal_cut_file_name, 'w')
+    template_file    = open(template_file_name   , 'r')
+    nominal_cut_file = open(nominal_cut_file_name, 'w')
+    fake_cut_file    = open(fake_cut_file_name   , 'w')
+    signal_cut_file  = open(signal_cut_file_name , 'w')
+    cf_cut_file      = open(signal_cut_file_name , 'w')
 
     for line in template_file:
         if '#' in line: continue
 
         if 'end' in line:
-            base_cut_file.write(  '\n')
-            fake_cut_file.write(  '\n')
-            signal_cut_file.write('\n')
+            nominal_cut_file.write('\n')
+            fake_cut_file.write(   '\n')
+            signal_cut_file.write( '\n')
+            cf_cut_file.write(     '\n')
 
-            base_cut_file.write(  '    pass_2_signal_leptons:     1\n')
-            fake_cut_file.write(  '    pass_2_signal_leptons:     0\n')
-            signal_cut_file.write('    pass_2_signal_leptons:     1\n')
+            nominal_cut_file.write('    pass_2_signal_leptons:     1\n')
+            fake_cut_file.write(   '    pass_2_signal_leptons:     0\n')
+            signal_cut_file.write( '    pass_2_signal_leptons:     1\n')
+            cf_cut_file.write(     '    pass_2_signal_leptons:     1\n')
 
-            base_cut_file.write(  '    pass_prompt_leptons:    1\n')
-            fake_cut_file.write(  '    pass_prompt_leptons:    0\n')
-            signal_cut_file.write('    pass_prompt_leptons:    0\n')
+            nominal_cut_file.write('    pass_prompt_leptons:    1\n')
+            fake_cut_file.write(   '    pass_prompt_leptons:    0\n')
+            signal_cut_file.write( '    pass_prompt_leptons:    0\n')
+            cf_cut_file.write(     '    pass_prompt_leptons:    1\n')
 
-            base_cut_file.write(  '\n')
-            fake_cut_file.write(  '\n')
-            signal_cut_file.write('\n')
+            # pass_sign_channel:      ss
 
-            base_cut_file.write(  '    trigger_weight:  1\n')
-            fake_cut_file.write(  '    trigger_weight:  0\n')
-            signal_cut_file.write('    trigger_weight:  1\n')
+            nominal_cut_file.write('\n')
+            fake_cut_file.write(   '\n')
+            signal_cut_file.write( '\n')
+            cf_cut_file.write    ( '\n')
 
-            base_cut_file.write(  '    lepton_weight:   1\n')
-            fake_cut_file.write(  '    lepton_weight:   0\n')
-            signal_cut_file.write('    lepton_weight:   1\n')
+            nominal_cut_file.write('    trigger_weight:  1\n')
+            fake_cut_file.write(   '    trigger_weight:  0\n')
+            signal_cut_file.write( '    trigger_weight:  1\n')
+            cf_cut_file.write(     '    trigger_weight:  1\n')
 
-            base_cut_file.write(  '    b_tag_weight:    1\n')
-            fake_cut_file.write(  '    b_tag_weight:    0\n')
-            signal_cut_file.write('    b_tag_weight:    1\n')
+            nominal_cut_file.write('    lepton_weight:   1\n')
+            fake_cut_file.write(   '    lepton_weight:   0\n')
+            signal_cut_file.write( '    lepton_weight:   1\n')
+            cf_cut_file.write(     '    lepton_weight:   1\n')
+
+            nominal_cut_file.write('    b_tag_weight:    1\n')
+            fake_cut_file.write(   '    b_tag_weight:    0\n')
+            signal_cut_file.write( '    b_tag_weight:    1\n')
+            cf_cut_file.write(     '    b_tag_weight:    1\n')
 
             if 'mm' in file_base:
-                base_cut_file.write(  '    cf_weight:       0\n')
-                fake_cut_file.write(  '    cf_weight:       0\n')
-                signal_cut_file.write('    cf_weight:       0\n')
+                nominal_cut_file.write('    cf_weight:       0\n')
+                fake_cut_file.write(   '    cf_weight:       0\n')
+                signal_cut_file.write( '    cf_weight:       0\n')
+                cf_cut_file.write(     '    cf_weight:       0\n')
             else:
-                base_cut_file.write(  '    cf_weight:       1\n')
-                fake_cut_file.write(  '    cf_weight:       0\n')
-                signal_cut_file.write('    cf_weight:       1\n')
+                nominal_cut_file.write('    cf_weight:       0\n')
+                fake_cut_file.write(   '    cf_weight:       0\n')
+                signal_cut_file.write( '    cf_weight:       0\n')
+                cf_cut_file.write(     '    cf_weight:       1\n')
 
-            base_cut_file.write(  '    fake_weight:     0\n')
-            fake_cut_file.write(  '    fake_weight:     1\n')
-            signal_cut_file.write('    fake_weight:     0\n')
+            nominal_cut_file.write('    fake_weight:     0\n')
+            fake_cut_file.write(   '    fake_weight:     1\n')
+            signal_cut_file.write( '    fake_weight:     0\n')
+            cf_cut_file.write(     '    fake_weight:     0\n')
 
-        base_cut_file.write(  line)
-        fake_cut_file.write(  line)
-        signal_cut_file.write(line)
+        nominal_cut_file.write(line)
+        fake_cut_file.write(   line)
+        signal_cut_file.write( line)
+        cf_cut_file.write(     line)
 
     template_file.close()
-    base_cut_file.close()
+    nominal_cut_file.close()
     fake_cut_file.close()
     signal_cut_file.close()
+    cf_cut_file.close()
 
 # ==============================================================================
 if __name__ == '__main__':
