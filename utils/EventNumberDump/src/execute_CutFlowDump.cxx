64cdc68e45b5e37f1d263aaec38d8700e0bc9a8c
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-02-07 15:48:33 +0000

    adding EventNumberDump module
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@21835 4525493e-7705-40b1-a816-d608a930855b

---
+++ utils/EventNumberDump/src/execute_CutFlowDump.cxx
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <TFile.h>
+#include <TTree.h>
+
+#include "include/CutFlowDump.h"
+
+int main(int argc, char** argv)
+{
+  std::cout << "Dumping Cut flow\n";
+
+  TFile* f = new TFile(argv[1]);
+  TTree* t = static_cast<TTree*>(f->Get("output"));
+
+  CutFlowDump cfd(t);
+  cfd.Loop();
+
+  cfd.printToScreen();
+  cfd.printToFile("cf.root");
+
+  delete f;
+}
