790ffb596a74cd1a9768fbd65e49b010c04415f2
ehines <ehines@4525493e-7705-40b1-a816-d608a930855b>
2013-02-13 16:25:52 +0000

    updating submit scripts for p1328 samples
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@22006 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts/submit_drell_yan.sh
+++ scripts/grid_submit_scripts/submit_drell_yan.sh
@@ -13,24 +13,25 @@ EXT_FILES=''
 # = define list of input files =
 # ==============================
 # mc12 list
-ds_list=( "146830.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp0Excl_Mll10to60:mc12_8TeV.146830.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp0Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146831.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp1Excl_Mll10to60:mc12_8TeV.146831.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp1Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146832.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp2Excl_Mll10to60:mc12_8TeV.146832.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp2Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146833.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp3Excl_Mll10to60:mc12_8TeV.146833.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp3Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146834.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp4Excl_Mll10to60:mc12_8TeV.146834.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp4Excl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
-          "146835.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp5Incl_Mll10to60:mc12_8TeV.146835.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp5Incl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
-          "146840.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp0Excl_Mll10to60:mc12_8TeV.146840.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp0Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146841.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp1Excl_Mll10to60:mc12_8TeV.146841.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp1Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146842.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp2Excl_Mll10to60:mc12_8TeV.146842.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp2Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146843.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp3Excl_Mll10to60:mc12_8TeV.146843.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp3Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146844.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp4Excl_Mll10to60:mc12_8TeV.146844.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp4Excl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
-          "146845.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp5Incl_Mll10to60:mc12_8TeV.146845.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp5Incl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
-          "146850.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp0Excl_Mll10to60:mc12_8TeV.146850.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp0Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146851.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp1Excl_Mll10to60:mc12_8TeV.146851.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp1Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146852.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp2Excl_Mll10to60:mc12_8TeV.146852.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp2Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146853.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp3Excl_Mll10to60:mc12_8TeV.146853.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp3Excl_Mll10to60.merge.NTUP_SUSY.e1254_s1469_s1470_r3542_r3549_p1181/"
-          "146854.AlpgenJimmy_Auto_AUET3CTEQ6L1_ZtautauNp4Excl_Mll10to60:mc12_8TeV.146854.AlpgenJimmy_Auto_AUET3CTEQ6L1_ZtautauNp4Excl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
-          "146855.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp5Incl_Mll10to60:mc12_8TeV.146855.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp5Incl_Mll10to60.merge.NTUP_SUSY.e1274_s1469_s1470_r3542_r3549_p1181/"
+ds_list=( "146830.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp0Excl_Mll10to60:mc12_8TeV.146830.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp0Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146831.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp1Excl_Mll10to60:mc12_8TeV.146831.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp1Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146832.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp2Excl_Mll10to60:mc12_8TeV.146832.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp2Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146833.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp3Excl_Mll10to60:mc12_8TeV.146833.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp3Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146834.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp4Excl_Mll10to60:mc12_8TeV.146834.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp4Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+#"146835.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp5Excl_Mll10to60:mc12_8TeV.146835.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZeeNp5Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146840.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp0Excl_Mll10to60:mc12_8TeV.146840.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp0Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146841.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp1Excl_Mll10to60:mc12_8TeV.146841.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp1Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146842.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp2Excl_Mll10to60:mc12_8TeV.146842.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp2Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146843.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp3Excl_Mll10to60:mc12_8TeV.146843.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp3Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146844.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp4Excl_Mll10to60:mc12_8TeV.146844.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp4Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+#"146845.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp5Excl_Mll10to60:mc12_8TeV.146845.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZmumuNp5Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/
+          "146850.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp0Excl_Mll10to60:mc12_8TeV.146850.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp0Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146851.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp1Excl_Mll10to60:mc12_8TeV.146851.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp1Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146852.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp2Excl_Mll10to60:mc12_8TeV.146852.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp2Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+          "146853.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp3Excl_Mll10to60:mc12_8TeV.146853.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp3Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+#"146854.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp4Excl_Mll10to60:mc12_8TeV.146854.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp4Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+#"146855.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp5Excl_Mll10to60:mc12_8TeV.146855.AlpgenJimmy_Auto_AUET2CTEQ6L1_ZtautauNp5Excl_Mll10to60.merge.NTUP_SUSY.e1600_s1499_s1504_r3658_r3549_p1328/"
+
         )
 
 . ${PWD}/submit_presel.sh
