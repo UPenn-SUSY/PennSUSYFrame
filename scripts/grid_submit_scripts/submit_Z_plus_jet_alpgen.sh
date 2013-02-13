b8128c471407791891a9deb4691eaa6ed703e0dc
ehines <ehines@4525493e-7705-40b1-a816-d608a930855b>
2013-02-13 14:06:02 +0000

    zjets container fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@21989 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts/submit_Z_plus_jet_alpgen.sh
+++ scripts/grid_submit_scripts/submit_Z_plus_jet_alpgen.sh
@@ -31,10 +31,10 @@ ds_list=( "107650.AlpgenJimmy_AUET2CTEQ6L1_ZeeNp0:mc12_8TeV.107650.AlpgenJimmy_A
           ##"107673.AlpgenJimmy_AUET2CTEQ6L1_ZtautauNp3:
           ##"107674.AlpgenJimmy_AUET2CTEQ6L1_ZtautauNp4:
           ##"107675.AlpgenJimmy_AUET2CTEQ6L1_ZtautauNp5:
-          ##"109300.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp0:
-          ##"109301.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp1:
-          ##"109302.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp2:
-          ##"109303.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp3:
+          "109300.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp0:mc12_8TeV.109300.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp0.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+          "109301.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp1:mc12_8TeV.109301.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp1.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+          "109302.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp2:mc12_8TeV.109302.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp2.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+          "109303.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp3:mc12_8TeV.109303.AlpgenJimmy_AUET2CTEQ6L1_ZeebbNp3.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"          
           ##"109310.AlpgenJimmy_AUET2CTEQ6L1_ZtautaubbNp0:
           ##"109311.AlpgenJimmy_AUET2CTEQ6L1_ZtautaubbNp1:
           ##"109312.AlpgenJimmy_AUET2CTEQ6L1_ZtautaubbNp2:
@@ -43,10 +43,10 @@ ds_list=( "107650.AlpgenJimmy_AUET2CTEQ6L1_ZeeNp0:mc12_8TeV.107650.AlpgenJimmy_A
           ##"117707.AlpgenJimmy_AUET2CTEQ6L1_ZtautauccNp1:
           ##"117708.AlpgenJimmy_AUET2CTEQ6L1_ZtautauccNp2:
           ##"117709.AlpgenJimmy_AUET2CTEQ6L1_ZtautauccNp3:
-          ##"126414.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp0:
-          ##"126415.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp1:
-          ##"126416.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp2:
-          ##"126417.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp3:
+          "126415.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp1:mc12_8TeV.126415.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp1.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+          "126416.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp2:mc12_8TeV.126416.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp2.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+          "126417.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp3:mc12_8TeV.126417.AlpgenJimmy_AUET2CTEQ6L1_ZeeccNp3.merge.NTUP_SUSY.e1601_s1499_s1504_r3658_r3549_p1328/"
+
           ##"126418.AlpgenJimmy_AUET2CTEQ6L1_ZmumuccNp0:
           ##"126419.AlpgenJimmy_AUET2CTEQ6L1_ZmumuccNp1:
           ##"126420.AlpgenJimmy_AUET2CTEQ6L1_ZmumuccNp2:
