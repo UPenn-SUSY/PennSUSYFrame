302c31b11b3cae7193aaf830714f90dcbc150f4e
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-02-18 10:27:17 +0000

    update grid submission scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@22137 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts/gen_job_config.py
+++ scripts/grid_submit_scripts/gen_job_config.py
@@ -32,6 +32,16 @@ def parseInputs():
                      , action = 'store_true'
                      , default = False
                      )
+    parser.add_option( '--presel'
+                     , dest = 'is_presel'
+                     , action = 'store_true'
+                     , default = True
+                     )
+    parser.add_option( '--cutflow'
+                     , dest = 'is_presel'
+                     , action = 'store_false'
+                     , default = True
+                     )
 
     return parser
 
@@ -42,17 +52,26 @@ def writeYmlFile(parser):
 
     user_config_file = ''
     if inputs.type == 'data':
-        user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.presel.50/user_config.presel.data.yml'
+        if inputs.is_presel:
+            user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.presel.50/user_config.presel.data.yml'
+        else:
+            user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.tnt.001/user_config.cut_flow.data.yml'
     else:
-        user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.presel.50/user_config.presel.mc.yml'
+        if inputs.is_presel:
+            user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.presel.50/user_config.presel.mc.yml'
+        else:
+            user_config_file = '${SFRAME_DIR}/../run/grid.00.02.05.tnt.001/user_config.cut_flow.mc.yml'
 
     f.write('grid:             True\n')
-    f.write('CycleName:        SusyDiLeptonPreselCycle\n')
+    f.write('CycleName:        SusyDiLeptonCutFlowCycle\n')
     f.write('SampleName:       %s\n' % inputs.sample_name)
     f.write('input_tree_name:  susy\n')
     f.write('is_af2:           %s\n' % inputs.is_af2)
     f.write('is_egamma_stream: %s\n' % inputs.is_egamma)
-    f.write('output_tree_name: presel\n')
+    if inputs.is_presel:
+        f.write('output_tree_name: presel\n')
+    else:
+        f.write('output_tree_name: output\n')
     f.write('Type:             %s\n' % inputs.type)
     f.write('\n')
 
