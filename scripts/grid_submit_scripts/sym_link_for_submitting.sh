f149ba36ea846e65eae212259993a80ed75d111a
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-03-14 16:14:48 +0000

    added script to symbolic link submission scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@22445 4525493e-7705-40b1-a816-d608a930855b

---
+++ scripts/grid_submit_scripts/sym_link_for_submitting.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+file_to_link=( \
+  gen_job_config.py  \
+  grid_build.sh  \
+  grid_run_cutflow.sh  \
+  grid_run_presel.sh  \
+  prep_cutflow_tarball.sh  \
+  prep_presel_tarball.sh  \
+  submit_cutflow.sh  \
+  submit_diboson.sh  \
+  submit_drell_yan.sh  \
+  submit_egamma_data.sh  \
+  submit_muon_data.sh  \
+  submit_pmssm_signal_grids.sh  \
+  submit_presel.sh  \
+  submit_signal_grids.sh  \
+  submit_simplified_signal_grids.sh \
+  submit_single_top.sh \
+  submit_ttbar.sh \
+  submit_W_plus_jet.sh \
+  submit_Z_plus_jet_alpgen.sh \
+  submit_Z_plus_jet.sh \
+  )
+
+for ftl in ${file_to_link[@]}
+do
+  echo $ftl
+  ln -s ${PWD}/${ftl} ${SFRAME_DIR}/..
+done
