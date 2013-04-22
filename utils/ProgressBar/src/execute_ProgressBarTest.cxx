03abbd7f75677502c6de70bf64ff99fac5c6c6fc
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-04-22 13:58:41 +0000

    update make files to be more flexible
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@23012 4525493e-7705-40b1-a816-d608a930855b

---
+++ utils/ProgressBar/src/execute_ProgressBarTest.cxx
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <TFile.h>
+#include <TTree.h>
+
+// #include "include/PlotNtupleMaker.h"
+// #include "Selection/include/EventSelection.h"
+// #include "Selection/include/LumiWeight.h"
+// 
+// #include "Parser/include/MasterConfigParser.h"
+// #include "Parser/include/CutConfigParser.h"
+// #include "Parser/include/HistConfigParser.h"
+
+// -----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  std::cout << "Testing progress bar!\n";
+}
