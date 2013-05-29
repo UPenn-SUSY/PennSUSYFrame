f725460783cd498e23b1f854e496ea4522988724
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-05-29 19:01:02 +0000

    update GenerateAllRunConfigFiles script
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@23509 4525493e-7705-40b1-a816-d608a930855b

--- utils/RunConfigs/scripts/GenerateAllRunConfigFiles.sh
+++ utils/RunConfigs/scripts/GenerateAllRunConfigFiles.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+configs_to_gen=''
+for var in $@
+do
+  if [[ ${var} == '--all' ]]
+  then
+    configs_to_gen=''
+    break
+  fi
+  configs_to_gen="${configs_to_gen} ${var}"
+done
+
+if [[ ${configs_to_gen} == '' ]]
+then
+  configs_to_gen='data fake nominal signal cf'
+fi
+
 if [[ -e config_list__${DATE_STR}.master.txt ]]
 then
   rm config_list__${DATE_STR}.master.txt
@@ -9,8 +25,9 @@ then
   rm config_list__${DATE_STR}.merge.master.txt
 fi
 
-# for type in cf data fake nominal signal
-for type in data fake nominal signal cf
+# # for type in cf data fake nominal signal
+# for type in data fake nominal signal cf
+for type in ${configs_to_gen}
 do
   echo $type
   DATE_STR=$(date +%Y_%m_%d)
@@ -23,5 +40,3 @@ do
   cat config_list__${DATE_STR}.${type}.txt       >> config_list__${DATE_STR}.master.txt
   cat config_list__${DATE_STR}.merge.${type}.txt >> config_list__${DATE_STR}.merge.master.txt
 done 
-
-
