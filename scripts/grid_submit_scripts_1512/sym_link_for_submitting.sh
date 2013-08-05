fcda5789d82fad7378d15e4f7e97993db901b26e
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-08-05 14:44:41 +0000

    up sym_link_for_submitting.sh
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@24405 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts_1512/sym_link_for_submitting.sh
+++ scripts/grid_submit_scripts_1512/sym_link_for_submitting.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# ==============================================================================
+# = Quick script to link all the relevant files to the top SusyFrame directory
+# = This should be run from the grid_submit_scripts directory (i.e. where the
+# = files actually live
+# ==============================================================================
+
 
 file_to_link=( \
   gen_job_config.py  \
@@ -7,27 +13,26 @@ file_to_link=( \
   grid_run_presel.sh  \
   prep_cutflow_tarball.sh  \
   prep_presel_tarball.sh  \
+
   submit_all_samples.sh  \
   submit_cutflow.sh  \
+  submit_presel.sh  \
 
-submit_ttbar_MadGraphPythia.sh
-submit_ttbar_McAtNloJimmy.sh
-submit_ttbar_PowhegPythia.sh
-submit_single_top_AcerMCPythia.sh
-submit_single_top_McAtNloJimmy.sh
-
-
+  submit_W_plus_jet.sh \
+  submit_Z_plus_jet.sh \
+  submit_Z_plus_jet_alpgen.sh \
   submit_diboson.sh  \
   submit_drell_yan.sh  \
   submit_egamma_data.sh  \
   submit_muon_data.sh  \
   submit_pmssm_signal_grids.sh  \
-  submit_presel.sh  \
   submit_simplified_wA_signal_grids.sh \
   submit_simplified_wC_signal_grids.sh \
-  submit_W_plus_jet.sh \
-  submit_Z_plus_jet_alpgen.sh \
-  submit_Z_plus_jet.sh \
+  submit_single_top_AcerMCPythia.sh
+  submit_single_top_McAtNloJimmy.sh
+  submit_ttbar_MadGraphPythia.sh
+  submit_ttbar_McAtNloJimmy.sh
+  submit_ttbar_PowhegPythia.sh
   )
 
 for ftl in ${file_to_link[@]}
