d54bd9c7cbfbeb1e45d419871f9baf839a051864
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-08-05 16:16:36 +0000

    up grid submit scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@24409 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts_1512/submit_WW__MadGraphPythia.sh
+++ scripts/grid_submit_scripts_1512/submit_WW__MadGraphPythia.sh
@@ -15,7 +15,7 @@ EXT_FILES=''
 # mc12 list 
 ds_list=(
           # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-          "167006:mc12_8TeV.167006.MadGraphPythia_AUET2BCTEQ6L1_WWWStar_lnulnulnu.merge.NTUP_SUSY.e1447_s1499_s1504_r3658_r3549_p1512/"
+          "167006.MadGraphPythia_AUET2BCTEQ6L1_WWWStar_lnulnulnu:mc12_8TeV.167006.MadGraphPythia_AUET2BCTEQ6L1_WWWStar_lnulnulnu.merge.NTUP_SUSY.e1447_s1499_s1504_r3658_r3549_p1512/"
         )
 
 . ${PWD}/submit_cutflow.sh
