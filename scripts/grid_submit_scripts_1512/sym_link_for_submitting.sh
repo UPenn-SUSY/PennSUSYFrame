2ddbbe251ec33a631596e76f55432915134c8c52
bjackson <bjackson@4525493e-7705-40b1-a816-d608a930855b>
2013-08-05 17:16:57 +0000

    up
    git-svn-id: svn+ssh://svn.cern.ch/reps/penn/bjackson/Susy2.0/trunk@24414 4525493e-7705-40b1-a816-d608a930855b

--- scripts/grid_submit_scripts_1512/sym_link_for_submitting.sh
+++ scripts/grid_submit_scripts_1512/sym_link_for_submitting.sh
@@ -7,32 +7,40 @@
 
 
 file_to_link=( \
-  gen_job_config.py  \
-  grid_build.sh  \
-  grid_run_cutflow.sh  \
-  grid_run_presel.sh  \
-  prep_cutflow_tarball.sh  \
-  prep_presel_tarball.sh  \
+  # helper scripts \
+  submit_all_samples.sh \
+  submit_cutflow.sh \
+  submit_presel.sh \
 
-  submit_all_samples.sh  \
-  submit_cutflow.sh  \
-  submit_presel.sh  \
+  # data submit scripts \
+  submit_egamma_data.sh \
+  submit_muon_data.sh \
 
-  submit_W_plus_jet.sh \
-  submit_Z_plus_jet.sh \
+  # background submit scripts \
+  submit_W_plus_jet__AlpgenJimmy.sh \
+  submit_higgs__PowhegPythia.sh \
+  submit_higgs__Pythia.sh \
+  submit_single_top__McAtNloJimmy.sh \
+  submit_triboson__MadGraphPythia.sh \
+  submit_ttbar__MadGraphPythia.sh \
+  submit_ttbar__McAtNloJimmy.sh \
+  submit_WW__Jimmy.sh \
+  submit_WW__MadGraphPythia.sh \
+  submit_WW__PowhegPythia8.sh \
+  submit_WW__Sherpa.sh \
+  submit_WZ__PowhegPythia8.sh \
   submit_Z_plus_jet_alpgen.sh \
-  submit_diboson.sh  \
-  submit_drell_yan.sh  \
-  submit_egamma_data.sh  \
-  submit_muon_data.sh  \
-  submit_pmssm_signal_grids.sh  \
+  submit_Z_plus_jet__Sherpa.sh \
+  submit_ZX__AlpgenPythia.sh \
+  submit_ZX__Sherpa.sh \
+  submit_ZZ__Jimmy.sh \
+  submit_ZZ__PowhegPythia8.sh \
+  submit_ZZ__Sherpa.sh \
+
+  # signal submit scripts \
+  submit_pmssm_signal_grids.sh \
   submit_simplified_wA_signal_grids.sh \
   submit_simplified_wC_signal_grids.sh \
-  submit_single_top_AcerMCPythia.sh
-  submit_single_top_McAtNloJimmy.sh
-  submit_ttbar_MadGraphPythia.sh
-  submit_ttbar_McAtNloJimmy.sh
-  submit_ttbar_PowhegPythia.sh
   )
 
 for ftl in ${file_to_link[@]}
@@ -40,3 +48,7 @@ do
   echo $ftl
   ln -s ${PWD}/${ftl} ${SFRAME_DIR}/..
 done
+
+
+
+
