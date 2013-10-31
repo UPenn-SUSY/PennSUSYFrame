425b9cf5ec48013cbb37032e7edbca5c37c623fa
bdjackson <bjack3@gmail.com>
2013-10-31 16:40:45 +0100

    compiles with SUSYTools-00-03-13

--- get_supporting_packages.sh
+++ get_supporting_packages.sh
@@ -6,39 +6,57 @@ echo "Checking out SFrame package"
 svn co https://svn.code.sf.net/p/sframe/code/SFrame/tags/SFrame-03-06-21/ SFrame
 mkdir SFrame/lib
 
-# ----------------------------------------------------------------------------
-# RootCore package
-echo "Checking out RootCore package"
+# ------------------------------------------------------------------------------
+# SUSYTools
+echo "Checking out SUSYTools"
 mkdir RootCore
 cd RootCore
 if [ "x$CERN_USER" = "x" ]; then
-  svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/D3PDTools/RootCore/tags/RootCore-00-01-95 RootCore
+  svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-03-13 SUSYTools
 else
-  svn co svn+ssh://${CERN_USER}@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/D3PDTools/RootCore/tags/RootCore-00-01-95 RootCore
+  svn co svn+ssh://${CERN_USER}@svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-03-13 SUSYTools
 fi
-cd RootCore
+python SUSYTools/python/install.py
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-echo "Configuring RootCore package"
+# ------------------------------------------------------------------------------
+# Setup RootCore
+echo "Setting up RootCore"
+cd RootCore
 ./configure
-cat Makefile-common | sed 's/-install_name @rpath\/lib$(PACKAGE).so/-undefined dynamic_lookup/g' > Makefile-common-temp
-cp Makefile-common-temp Makefile-common
+source scripts/setup.sh
+cd ..
+$ROOTCOREDIR/scripts/find_packages.sh
+$ROOTCOREDIR/scripts/compile.sh
+$ROOTCOREDIR/scripts/build.sh
+cd ..
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cd ../..
 source setup_sframe.sh
-cd RootCore
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # check out RootCore packages
 echo "Check out RootCore modules"
-$ROOTCOREDIR/scripts/checkout.sh ../root_core_packages
+cd ${ROOTCOREDIR}/..
+for module in $(cat ../root_core_packages) ; do
+  echo "module: $module"
+  module_short=$(echo $module | sed 's#-.*##g' | sed 's#.*/##g')
+  if [[ $module_short == "trunk" ]] ; then
+    module_short=$(echo $module | sed "s#/trunk##g" | sed "s#.*/##g")
+  fi
+  echo "module short: $module_short"
+  if [ "x$CERN_USER" = "x" ]; then
+    svn co svn+ssh://svn.cern.ch/reps/$module $module_short
+  else
+    svn co svn+ssh://${CERN_USER}@svn.cern.ch/reps/$module $module_short
+  fi
+done
+cd ${SFRAME_DIR}/..
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # comment out known std::cout statements to avoid massive log files
-files="GoodRunsLists/Root/DQHelperFunctions.cxx
-GoodRunsLists/Root/TGoodRunsList.cxx
-SUSYTools/Root/mt2_bisect.cxx
-LeptonTruthTools/Root/RecoTruthMatch.cxx"
+files="GoodRunsLists/Root/TGoodRunsList.cxx
+       SUSYTools/Root/mt2_bisect.cxx
+       LeptonTruthTools/Root/RecoTruthMatch.cxx"
 for f in $files
 do
   # cat $f | sed 's, std::cout, \/\/ std::cout,g' > $f.temp
@@ -49,6 +67,16 @@ done
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # build RootCore packages
 cd ${ROOTCOREDIR}/..
-${ROOTCOREDIR}/scripts/build.sh
+echo "Preparing to build RootCore packages"
+$ROOTCOREDIR/scripts/find_packages.sh
+$ROOTCOREDIR/scripts/compile.sh
+$ROOTCOREDIR/scripts/build.sh
+
+echo "Build finished! Moving RootCore.par to proper location"
+cd ${ROOTCOREDIR}/..
 mv RootCore.par ${SFRAME_LIB_PATH}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+echo "Back to SFRAME_DIR"
 cd ${SFRAME_DIR}/..
+
