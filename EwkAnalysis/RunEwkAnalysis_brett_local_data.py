73e6d05a26b5d18a690ad2cf5af2e738c3d15c47
bdjackson <bjack3@gmail.com>
2014-02-07 16:02:18 +0100

    add scripts to run over files on my laptop

---
+++ EwkAnalysis/RunEwkAnalysis_brett_local_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+import optparse
+import time
+
+import ROOT
+import rootlogon
+import metaroot
+
+# ==============================================================================
+ROOT.gROOT.ProcessLine(".x ${ROOTCOREDIR}/scripts/load_packages.C")
+ROOT.gSystem.Load('${BASE_WORK_DIR}/lib/libProgressBar.so')
+ROOT.gSystem.Load('${BASE_WORK_DIR}/lib/libCutFlowTracker.so')
+ROOT.gSystem.Load('${BASE_WORK_DIR}/lib/libPennSusyFrameCore.so')
+ROOT.gSystem.Load('${BASE_WORK_DIR}/lib/libEwkAnalysis.so')
+
+# ==============================================================================
+file_list = [ "/Users/bjackson/work/data12/NTUP_SUSY.01267663._000001.root.1"
+            ]
+
+t = ROOT.TChain('susy')
+for fl in file_list:
+    t.AddFile(fl)
+
+# ==============================================================================
+ewa = ROOT.PennSusyFrame.EwkAnalysis(t)
+ewa.setIsData()
+ewa.prepareTools()
+ewa.Loop()
+
+# ==============================================================================
+print ''
+print ''
