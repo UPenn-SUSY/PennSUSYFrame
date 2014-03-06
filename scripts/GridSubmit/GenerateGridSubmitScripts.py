c19768448e44f2bffc182a767306b5e89b564d7a
bdjackson <bjack3@gmail.com>
2014-03-06 16:09:41 +0100

    update for TNT_102

--- scripts/GridSubmit/GenerateGridSubmitScripts.py
+++ scripts/GridSubmit/GenerateGridSubmitScripts.py
@@ -90,10 +90,10 @@ def submitFromList( ds_dict_list, tnt_tag, starting_num):
 
 # ==============================================================================
 if __name__ == '__main__':
-    tnt_tag = 'tnt_100'
+    tnt_tag = 'tnt_102'
 
     # ------------------------------------------------------------------------------
-    # generateTarBallScript(tnt_tag = tnt_tag)
+    generateTarBallScript(tnt_tag = tnt_tag)
 
     # ------------------------------------------------------------------------------
     job_num = 0
