4248e6ef65a45da4a3562624051990451cdda73d
bdjackson <bjack3@gmail.com>
2014-08-21 16:51:16 +0200

    add script to divide datasets between users for eos copy

--- scripts/GridSubmit/DivideDatasets.py
+++ scripts/GridSubmit/DivideDatasets.py
@@ -13,16 +13,29 @@ class UserData(object):
         self.current_size = 0
         self.ds_list = []
 
-    def __add__(self, x):
-        print '%s plus %s' % (self, x)
+    def addSample(self, ds):
+        self.ds_list.append(ds)
+        self.current_size += ds.size
 
-        if not isinstance(x, UserData):
-            print 'self: %s -- x: %s' % (self, x)
-            return x+self.max_size
+    def printUserInfo(self):
+        print 'user name: ' , self.user_name
+        print '  max size: ' , self.max_size
+        print '  allocated size: ' , self.current_size
+        print '  number samples: ' , len(self.ds_list)
 
-        print 'adding UserData'
-        print 'self: %s -- x: %s' % (self, x)
-        return self.max_size + x.max_size
+    def printUserDataSets(self):
+        self.printUserInfo()
+        print '  DS list:'
+        for ds in self.ds_list:
+            print '    %s' % ds.name
+        print ''
+
+# ==============================================================================
+class DataSet(object):
+    def __init__(self, ds_name, num_files, ds_size):
+        self.name      = ds_name
+        self.num_files = num_files
+        self.size      = ds_size
 
 # ------------------------------------------------------------------------------
 def getDSList(ds_list_file_name):
@@ -30,22 +43,51 @@ def getDSList(ds_list_file_name):
 
     f = file(ds_list_file_name)
     for l in f.readlines():
-        print l
-
+        splits = l.split()
+        ds_list.append( DataSet( splits[0]
+                               , int(splits[1])
+                               , float(splits[2])
+                               )
+                      )
     return ds_list
 
 # ------------------------------------------------------------------------------
+def fillUserLists(user_list, ds_list):
+    # loop over data-sets
+    for ds in ds_list:
+        ds_allocated = False
+
+        # look at user list. assign this ds to the first user who can hold it
+        for user in user_list:
+            if user.current_size + ds.size < user.max_size:
+                print 'Adding sample %s to user %s' % (ds.name, user.user_name)
+                user.addSample(ds)
+                ds_allocated = True
+                break
+        if not ds_allocated:
+            print "ERROR: We don't have space for sample " , ds.name
+            for user in user_list:
+                user.printUserInfo()
+                print ''
+
+# ------------------------------------------------------------------------------
 def main():
-    user_list = [ UserData('brett'  , 500)
+    user_list = [
+                  UserData('evelyn' , 800)
+                , UserData('brett'  , 500)
                 , UserData('liz'    , 500)
                 , UserData('leigh'  , 700)
                 , UserData('emma'   , 700)
-                , UserData('evelyn' , 800)
                 ]
     total_space = reduce( lambda a, b: a+b , [x.max_size for x in user_list] )
     print 'total available space: ' , total_space
 
     ds_list = getDSList('dataset_sizes.tnt_105.txt')
+
+    fillUserLists(user_list, ds_list)
+
+    for user in user_list:
+        user.printUserDataSets()
 
 # ==============================================================================
 if __name__ == '__main__':
