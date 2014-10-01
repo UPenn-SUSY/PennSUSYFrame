46b515947dadead93deb08198c8ad00b1b73388f
bdjackson <bjack3@gmail.com>
2014-10-01 15:55:19 +0200

    update script to check for incomplete datasets

--- scripts/GridSubmit/CheckForIncompleteDatasets.py
+++ scripts/GridSubmit/CheckForIncompleteDatasets.py
@@ -7,10 +7,11 @@
 #===============================================================================
 
 import os
+import pprint
 
 #-------------------------------------------------------------------------------
 ami_data_file_name = "%s/data/ami_data12.txt" % os.environ['BASE_WORK_DIR']
-ami_data_file_name = "%s/data/ami_mc12.txt"   % os.environ['BASE_WORK_DIR']
+ami_mc_file_name   = "%s/data/ami_mc12.txt"   % os.environ['BASE_WORK_DIR']
 
 #-------------------------------------------------------------------------------
 def processDataLine(line):
@@ -18,24 +19,35 @@ def processDataLine(line):
     splits = cleaned_line.split()
 
     dataset = splits[0]
-    period  = splits[1]
-    total_events = splits[2]
-    num_files    = splits[3]
-    total_size   = splits[4]
 
-    dataset_splits = dataset.split('.')
-    run_num = dataset_splits[1]
-    stream  = dataset_splits[2]
-    tag     = dataset_splits[-1]
+    if not dataset == 'DATASET':
+        period       = splits[1]
+        total_events = splits[2]
+        num_files    = splits[3]
+        total_size   = splits[4]
+
+        dataset_splits = dataset.split('.')
+        run_num  = dataset_splits[1]
+        stream   = dataset_splits[2]
+        prod_tag = dataset_splits[-1].split('_')[-1]
+    else:
+        period       = ""
+        total_events = 0
+        num_files    = 0
+        total_size   = 0
+
+        run_num  = ""
+        stream   = ""
+        prod_tag = ""
 
     return { 'dataset':dataset
            , 'period':period
-           , 'total_events':total_events
-           , 'num_files':num_files
-           , 'total_size':total_size
+           , 'total_events':int(total_events)
+           , 'num_files':int(num_files)
+           , 'total_size':float(total_size)
            , 'run_num':run_num
            , 'stream':stream
-           , 'tag':tag
+           , 'prod_tag':prod_tag
            }
 
 #-------------------------------------------------------------------------------
@@ -44,28 +56,77 @@ def processMCLine(line):
     splits = cleaned_line.split()
 
     dataset = splits[0]
-    total_events = splits[2]
-    num_files    = splits[3]
-    total_size   = splits[4]
 
-    dataset_splits = dataset.split('.')
-    dsid    = dataset_splits[1]
-    ds_name = dataset_splits[2]
-    tag     = dataset_splits[-1]
+    if not dataset == "DATASET":
+        total_events = splits[2]
+        num_files    = splits[3]
+        total_size   = splits[4]
+
+        dataset_splits = dataset.split('.')
+        dsid     = dataset_splits[1]
+        ds_name  = dataset_splits[2]
+        prod_tag = dataset_splits[-1]
+    else:
+        total_events = 0
+        num_files    = 0
+        total_size   = 0
+
+        dsid     = ""
+        ds_name  = ""
+        prod_tag = ""
 
     return { 'dataset':dataset
-           , 'period':period
-           , 'total_events':total_events
-           , 'num_files':num_files
-           , 'total_size':total_size
+           , 'period':'MC'
+           , 'total_events':int(total_events)
+           , 'num_files':int(num_files)
+           , 'total_size':float(total_size)
            , 'dsid':dsid
            , 'ds_name':ds_name
-           , 'tag':tag
+           , 'prod_tag':prod_tag
            }
 
 #-------------------------------------------------------------------------------
+def getDatasetSizes(is_data):
+    f = file(ami_data_file_name if is_data else ami_mc_file_name)
+
+    data_size_dict = {}
+    for l in f.readlines():
+        processed_line = processDataLine(l) if is_data else processMCLine(l)
+        if processed_line['dataset'] == 'DATASET': continue
+
+        line_tag = 'data__' if is_data else 'mc__'
+
+        if is_data:
+            line_tag += processed_line['period'].strip('1234567890')
+            line_tag += '__'
+            line_tag += processed_line['stream']
+        else:
+            line_tag += processed_line['dsid']
+            line_tag += '__'
+            line_tag += processed_line['ds_name']
+        line_tag += '__'
+        line_tag += processed_line['prod_tag']
+
+        if line_tag not in data_size_dict:
+            data_size_dict[line_tag] = { 'total_events':0
+                                       , 'num_files':0
+                                       , 'total_size':0
+                                       }
+        data_size_dict[line_tag]['total_events'] += processed_line['total_events']
+        data_size_dict[line_tag]['num_files']    += processed_line['num_files']
+        data_size_dict[line_tag]['total_size']   += processed_line['total_size']
+
+    return data_size_dict
+
+#-------------------------------------------------------------------------------
 def main():
-    pass
+    data_sizes = getDatasetSizes(is_data = True )
+    mc_sizes   = getDatasetSizes(is_data = False)
+
+    pp = pprint.PrettyPrinter(indent = 2)
+    pp.pprint(data_sizes)
+    print ''
+    # pp.pprint(mc_sizes)
 
 #===============================================================================
 if __name__ == "__main__":
