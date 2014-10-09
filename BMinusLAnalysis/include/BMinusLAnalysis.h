4d8d8202b8b32616f589cf007b256473d5e8c5d2
bdjackson <bjack3@gmail.com>
2014-10-09 16:33:13 +0200

    remove dr study from BMinusLAnalysis -- to move it to a separate analyzer

--- BMinusLAnalysis/include/BMinusLAnalysis.h
+++ BMinusLAnalysis/include/BMinusLAnalysis.h
@@ -46,61 +46,9 @@ namespace PennSusyFrame
 
                                                    , "BMINUSL_N"
                                                    };
-      enum BMINUSL_DR_HIST_LEVELS { BMINUSL_NO_OR
-                                  , BMINUSL_EE_OR
-                                  , BMINUSL_EJ_OR
-                                  , BMINUSL_MJ_OR
-                                  , BMINUSL_JE_OR
-                                  , BMINUSL_JM_OR
-                                  , BMINUSL_EM_OR
-                                  , BMINUSL_MM_OR
-                                  , BMINUSL_DR_HIST_N
-                                  };
-
-      const std::string BMINUSL_DR_HIST_LEVEL_STRINGS[] = { "BMINUSL_NO_OR"
-                                                          , "BMINUSL_EE_OR"
-                                                          , "BMINUSL_EJ_OR"
-                                                          , "BMINUSL_MJ_OR"
-                                                          , "BMINUSL_JE_OR"
-                                                          , "BMINUSL_JM_OR"
-                                                          , "BMINUSL_EM_OR"
-                                                          , "BMINUSL_MM_OR"
-                                                          , "BMINUSL_DR_HIST_N"
-                                                          };
 
   // ---------------------------------------------------------------------------
   class blPair;
-
-  // ===========================================================================
-  class DRStudies
-  {
-    public:
-      DRStudies();
-      virtual ~DRStudies();
-
-      void init();
-      void finalizeRun(std::string);
-
-      void reproduceFullObjectCleaning( const PennSusyFrame::Event&
-                                      , const PennSusyFrame::MCTruth&
-                                      , PennSusyFrame::ObjectCleaning
-                                      , const std::vector<PennSusyFrame::Electron*>*
-                                      , const std::vector<PennSusyFrame::Muon*>*
-                                      , const std::vector<PennSusyFrame::Jet*>*
-                                      , const std::vector<PennSusyFrame::Jet*>*
-                                      );
-
-    protected:
-      void filldRHistHandles( BMINUSL_DR_HIST_LEVELS
-                            , const PennSusyFrame::Event&
-                            , const PennSusyFrame::MCTruth&
-                            , const std::vector<PennSusyFrame::Electron*>&
-                            , const std::vector<PennSusyFrame::Muon*>&
-                            , const std::vector<PennSusyFrame::Jet*>&
-                            );
-      std::vector<PennSusyFrame::DRHists*> m_bminusl_dr_histogram_handler;
-
-    };
 
   // ===========================================================================
   class BMinusLAnalysis : public PennSusyFrame::PennSusyFrameCore
@@ -250,9 +198,6 @@ namespace PennSusyFrame
 
       double m_min_b_jet_pt_baseline;
       double m_max_b_jet_pt_baseline;
-
-    private:
-      DRStudies m_dr;
   };
 }
 
--- BMinusLAnalysis/src/BMinusLAnalysis.cxx
+++ BMinusLAnalysis/src/BMinusLAnalysis.cxx
@@ -177,8 +177,6 @@ void PennSusyFrame::BMinusLAnalysis::beginRun()
       m_bminusl_detailed_histogram_handler.push_back(new PennSusyFrame::BMinusLDetailedHists(PennSusyFrame::BMINUSL_HIST_LEVEL_STRINGS[hist_level]));
     }
   }
-
-  m_dr.PennSusyFrame::DRStudies::init();
 }
 
 // -----------------------------------------------------------------------------
@@ -698,8 +696,6 @@ void PennSusyFrame::BMinusLAnalysis::finalizeRun()
     }
   }
 
-  m_dr.finalizeRun(m_out_hist_file_name);
-
   out_hist_file.Close();
   std::cout << "file is closed!\n";
 
@@ -762,11 +758,6 @@ bool PennSusyFrame::BMinusLAnalysis::passBMinusLTrigger()
   if (flavor_channel == FLAVOR_EM && m_trigger.getEF_e24vhi_medium1()) return true;
   if (flavor_channel == FLAVOR_EM && m_trigger.getEF_mu24i_tight())    return true;
   // if (flavor_channel == FLAVOR_EM && m_trigger.getEF_mu36_tight())     return true;
-
-  // if (flavor_channel == FLAVOR_EE && m_trigger.getEF_e24vh_medium1_e7_medium1()) return true;
-  // if (flavor_channel == FLAVOR_MM && m_trigger.getEF_mu18_tight_mu8_EFFS())      return true;
-  // if (flavor_channel == FLAVOR_EM && m_trigger.getEF_e12Tvh_medium1_mu8())       return true;
-  // if (flavor_channel == FLAVOR_EM && m_trigger.getEF_mu18_tight_e7_medium1())    return true;
 
   return false;
 }
@@ -971,17 +962,6 @@ void PennSusyFrame::BMinusLAnalysis::constructObjects()
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   m_event.setFlavorChannel(findFlavorChannel());
   m_event.setSignChannel(findSignCannel());
-
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  // replicate overlap removal to fill dR histograms
-  m_dr.PennSusyFrame::DRStudies::reproduceFullObjectCleaning( m_event
-                                                            , m_mc_truth
-                                                            , m_object_cleaning
-                                                            , m_electrons.getCollection(EL_BASELINE)
-                                                            , m_muons.getCollection(MU_BASELINE)
-                                                            , m_jets.getCollection(JET_BASELINE_GOOD)
-                                                            , m_jets.getCollection(JET_BASELINE_BAD)
-                                                            );
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   // compute mll
@@ -1281,15 +1261,6 @@ void PennSusyFrame::BMinusLAnalysis::fillHistHandles( PennSusyFrame::BMINUSL_HIS
                                                          , m_met
                                                          , weight
                                                          );
-    // m_histogram_handlers.at(hist_level).at(hist_it)->Fill( m_event
-    //                                                      , m_event_quantities
-    //                                                      , m_electrons.getCollection(EL_GOOD)
-    //                                                      , m_muons.getCollection(MU_GOOD)
-    //                                                      // , m_jets.getCollection(JET_SELECTED)
-    //                                                      , m_jets.getCollection(JET_B)
-    //                                                      , m_met
-    //                                                      , weight
-    //                                                      );
   }
 
   // fill b-l histograms
@@ -1328,150 +1299,4 @@ void PennSusyFrame::BMinusLAnalysis::fillHistHandles( PennSusyFrame::BMINUSL_HIS
                                                                       );
     }
   }
-}
-// -----------------------------------------------------------------------------
-// -------- DR Studies ---------------------------------------------------------
-// -----------------------------------------------------------------------------
-PennSusyFrame::DRStudies::DRStudies()
-{}
-
-PennSusyFrame::DRStudies::~DRStudies()
-{}
-
-void PennSusyFrame::DRStudies::init() {
-  m_bminusl_dr_histogram_handler.reserve(BMINUSL_DR_HIST_N);
-  for (unsigned int dr_hist_level=0; dr_hist_level != BMINUSL_DR_HIST_N; ++ dr_hist_level) {
-    m_bminusl_dr_histogram_handler.push_back(new PennSusyFrame::DRHists(BMINUSL_DR_HIST_LEVEL_STRINGS[dr_hist_level]));
-  }
-}
-
-void PennSusyFrame::DRStudies::finalizeRun(std::string m_out_hist_file_name) {
-  std::string file_name = m_out_hist_file_name + "__DR";
-  TFile out_hist_file(file_name.c_str(), "RECREATE");
-  
-  for (unsigned int dr_hist_level=0; dr_hist_level != PennSusyFrame::BMINUSL_DR_HIST_N; ++dr_hist_level) {
-    TDirectory* dr_hist_dir = out_hist_file.mkdir(PennSusyFrame::BMINUSL_DR_HIST_LEVEL_STRINGS[dr_hist_level].c_str());
-    m_bminusl_dr_histogram_handler.at(dr_hist_level)->write(dr_hist_dir);
-  }
-  out_hist_file.Close();
-}
-
-void PennSusyFrame::DRStudies::reproduceFullObjectCleaning(const PennSusyFrame::Event& m_event
-							   , const PennSusyFrame::MCTruth& m_mc_truth
-							   , PennSusyFrame::ObjectCleaning m_object_cleaning
-							   , const std::vector<PennSusyFrame::Electron*>* input_electrons
-							   , const std::vector<PennSusyFrame::Muon*>* input_muons
-							   , const std::vector<PennSusyFrame::Jet*>* input_jets_good
-							   , const std::vector<PennSusyFrame::Jet*>* input_jets_bad
-							   ) {
-  // fill hist with no OR
-  filldRHistHandles(PennSusyFrame::BMINUSL_NO_OR
-		    , m_event
-		    , m_mc_truth
-		    , *input_electrons
-		    , *input_muons
-		    , *input_jets_good
-		    );
-
-  // do ee overlap removal
-  std::vector<PennSusyFrame::Electron*> el_temp_1;
-  m_object_cleaning.eeOverlapRemoval(*input_electrons, el_temp_1);
-  filldRHistHandles(PennSusyFrame::BMINUSL_EE_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_1
-		    , *input_muons
-		    , *input_jets_good
-		    );
-
-  // do ej overlap removal
-  std::vector<PennSusyFrame::Jet*> jet_good_temp_1;
-  std::vector<PennSusyFrame::Jet*> jet_bad_temp_1;
-  m_object_cleaning.ejOverlapRemoval(el_temp_1, *input_jets_good, jet_good_temp_1);
-  m_object_cleaning.ejOverlapRemoval(el_temp_1, *input_jets_bad, jet_bad_temp_1);
-  filldRHistHandles(PennSusyFrame::BMINUSL_EJ_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_1
-		    , *input_muons
-		    , jet_good_temp_1
-		    );
-
-  // do mj overlap removal
-  std::vector<PennSusyFrame::Jet*> jet_good_temp_2;
-  std::vector<PennSusyFrame::Jet*> jet_bad_temp_2;
-  m_object_cleaning.mjOverlapRemoval(*input_muons, jet_good_temp_1, jet_good_temp_2);
-  m_object_cleaning.mjOverlapRemoval(*input_muons, jet_bad_temp_1, jet_bad_temp_2);
-  filldRHistHandles(PennSusyFrame::BMINUSL_MJ_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_1
-		    , *input_muons
-		    , jet_good_temp_2
-		    );
-
-  // do je overlap removal
-  std::vector<PennSusyFrame::Electron*> el_temp_2;
-  std::vector<PennSusyFrame::Electron*> el_temp_3;
-  m_object_cleaning.jeOverlapRemoval(jet_good_temp_2, el_temp_1, el_temp_2);
-  m_object_cleaning.jeOverlapRemoval(jet_bad_temp_2 , el_temp_2, el_temp_3);
-  filldRHistHandles(PennSusyFrame::BMINUSL_JE_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_2
-		    , *input_muons
-		    , jet_good_temp_2
-		    );
-
-  // do jm overlap removal
-  std::vector<PennSusyFrame::Muon*> mu_temp_1;
-  std::vector<PennSusyFrame::Muon*> mu_temp_2;
-  m_object_cleaning.jmOverlapRemoval(jet_good_temp_2, *input_muons, mu_temp_1);
-  m_object_cleaning.jmOverlapRemoval(jet_bad_temp_2 , mu_temp_1   , mu_temp_2);
-  filldRHistHandles(PennSusyFrame::BMINUSL_JM_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_2
-		    , mu_temp_1
-		    , jet_good_temp_2
-		    );
-
-  // do em overlap removal
-  std::vector<PennSusyFrame::Electron*> el_temp_4;
-  std::vector<PennSusyFrame::Muon*> mu_temp_3;
-  m_object_cleaning.emOverlapRemoval(el_temp_3, mu_temp_2, el_temp_4, mu_temp_3);
-  filldRHistHandles(PennSusyFrame::BMINUSL_EM_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_4
-		    , mu_temp_3
-		    , jet_good_temp_2
-		    );
-
-  // do mm overlap removal
-  std::vector<PennSusyFrame::Muon*> mu_temp_4;
-  m_object_cleaning.mmOverlapRemoval(mu_temp_3, mu_temp_4);
-  filldRHistHandles(PennSusyFrame::BMINUSL_MM_OR
-		    , m_event
-		    , m_mc_truth
-		    , el_temp_4
-		    , mu_temp_4
-		    , jet_good_temp_2
-		    );
-}
-
-void PennSusyFrame::DRStudies::filldRHistHandles(BMINUSL_DR_HIST_LEVELS hist_level
-						 , const PennSusyFrame::Event& m_event
-						 , const PennSusyFrame::MCTruth& m_mc_truth
-						 , const std::vector<PennSusyFrame::Electron*>& electrons
-						 , const std::vector<PennSusyFrame::Muon*>& muons
-						 , const std::vector<PennSusyFrame::Jet*>& jets
-						 )
-{
-  m_bminusl_dr_histogram_handler.at(hist_level)->FilldR( m_event
-							 , electrons
-							 , muons
-							 , jets
-							 , m_mc_truth
-                                                         );
 }
