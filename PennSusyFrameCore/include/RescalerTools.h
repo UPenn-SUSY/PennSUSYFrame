5dd6f4c6bb1104c33c0a85496c066d13d72f939b
bdjackson <bjack3@gmail.com>
2014-10-22 17:18:01 +0200

    add jes up/down. bugfix for jer

--- PennSusyFrameCore/include/RescalerTools.h
+++ PennSusyFrameCore/include/RescalerTools.h
@@ -33,6 +33,7 @@ namespace JetAnalysisCalib {
   class JetCalibrationTool;
 }
 class JetSmearingTool;
+class MultijetJESUncertaintyProvider;
 
 // =============================================================================
 namespace PennSusyFrame
@@ -102,8 +103,12 @@ namespace PennSusyFrame
                                    , int num_vertices_ge_2_tracks
                                    );
     // void applyJER(PennSusyFrame::Jet*, bool is_af2);
-    void applyJER(TLorentzVector&, bool is_af2);
-    void applyJES(TLorentzVector&, bool is_af2);
+    void applyJER(TLorentzVector&);
+    void applyJES( TLorentzVector&
+                 , int flavor_truth_label
+                 , int num_vert_ge_2_trk
+                 , float mu
+                 );
 
     private:
       bool m_is_data;
@@ -114,6 +119,7 @@ namespace PennSusyFrame
       bool m_do_jes_down;
       JetAnalysisCalib::JetCalibrationTool* m_jet_calibration;
       JetSmearingTool* m_jer_smearing;
+      MultijetJESUncertaintyProvider* m_jes_tool;
   };
 
   // =============================================================================
