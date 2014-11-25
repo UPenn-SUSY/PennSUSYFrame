8d6cd6f0420a5f23948355bb42cf2e5c18f8fcae
bdjackson <bjack3@gmail.com>
2014-11-25 11:23:02 +0100

    update plot to make sample p value plot

--- LimitHelpers/MakeSamplePValuePlot.py
+++ LimitHelpers/MakeSamplePValuePlot.py
@@ -1,117 +1,219 @@
 #!/usr/bin/env python
 
-# import pandas
 import glob
+import itertools
 
-import array
+import pandas
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['text.usetex']=True
+mpl.rcParams['text.latex.unicode']=True
 import ROOT
+
 import summary_harvest_tree_description as tree_summary
+
 
 ROOT.gStyle.SetOptStat(0)
 
-## # ------------------------------------------------------------------------------
-## def readHypoTestResults(input_file_name):
-##     the_tree = tree_summary.harvesttree(input_file_name)
-##     results = {}
-## 
-##     for entry in the_tree:
-##         results[int(entry.mst)] = entry.CLs
-## 
-##     return results
-
 # ------------------------------------------------------------------------------
-def extractBranchingRatios(file_name):
-    print file_name
+def extract_branching_ratios(file_name):
     splits = file_name.split('_')
-    return {'bre':int(splits[2])/100.,
-            'brm':int(splits[4])/100.,
-            'brt':int(splits[6])/100.}
+    return {'bre': int(splits[2]) / 100.,
+            'brm': int(splits[4]) / 100.,
+            'brt': int(splits[6]) / 100.}
+
 
 # ------------------------------------------------------------------------------
-def readHypoTestResults():
+def read_hypo_test_results():
+    results = pandas.DataFrame(columns=('mass', 'bre', 'brm', 'brt', 'cls'))
+
     file_list = glob.glob('SampleExcl_*_harvest_list')
     for fl in file_list:
-        branching_ratios = extractBranchingRatios(fl)
-        print branching_ratios
+        branching_ratios = extract_branching_ratios(fl)
 
         this_tree = tree_summary.harvesttree(fl)
         for entry in this_tree:
+            # extract the mass and CLS from this entry of the tree
             this_mass = int(entry.mst)
             this_cls = entry.CLs
 
-            print '  - mass: ', this_mass, ' -- cls: ', this_cls
-        print ''
-    pass
+            # create list for this data frame entry and append to the end
+            this_df_entry = [this_mass,
+                             branching_ratios['bre'],
+                             branching_ratios['brm'],
+                             branching_ratios['brt'],
+                             this_cls]
+            results.loc[results.shape[0]] = this_df_entry
+
+    return results.sort('mass')
+
 
 # ------------------------------------------------------------------------------
-def getMinMax(val_list, limit_min_to_zero = True):
+def get_min_max(val_list, limit_min_to_zero=True):
     min_val = min(val_list)
     max_val = max(val_list)
     delta = max_val - min_val
 
-    min_val -= 0.1*delta
-    max_val += 0.1*delta
+    min_val -= 0.1 * delta
+    max_val += 0.1 * delta
 
     if limit_min_to_zero:
         min_val = max(0., min_val)
 
     return (min_val, max_val)
 
-# ------------------------------------------------------------------------------
-# def makePValuePlot(input_file_name):
-#     # get results from hypothesis test
-#     results = readHypoTestResults(input_file_name)
 
-def makePValuePlot():
+# ------------------------------------------------------------------------------
+def plot_cls_triangle(result_df, out_file_name):
+    """
+    Function takes a data frame, with branching ratios and CLs values.
+    Constructs a triangle with showing the CLs for each branching ratio choice
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot([0,1], [1,0], color = '0.75', linestyle = '--')
+    my_plot = plt.hexbin( x = result_df['bre'],
+                          y = result_df['brt'],
+                          C = result_df['cls'],
+                          cmap = plt.cm.BuGn_r,
+                          reduce_C_function = np.max,
+                          gridsize = (25,5),
+                          vmin = 0.,
+                          vmax = 0.10)
+    plt.axis([0, 1.1, 0, 1.1])
+    plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
+    plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
+    plt.grid(True)
+
+    # add label with stop mass for this plot
+    label_string = ''.join(('Stop mass = ',
+                            str(result_df['mass'].iloc[0]),
+                            ' GeV'))
+    ax.text(0.65, 1.0, label_string,
+            bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
+
+    # add color bar to right side
+    cb = plt.colorbar(my_plot,spacing='uniform',extend='max')
+    cb.set_label('CL_{S}')
+
+    # write plot to file
+    plt.savefig(out_file_name)
+
+
+# ------------------------------------------------------------------------------
+def plot_mass_limit_triangle(result_df, out_file_name):
+    """
+    Function takes a data frame, with branching ratios, masses, and CLs values.
+    Constructs a triangle with showing the maximum mass which is excluded at
+    each point in the branching ratio triangle.
+    """
+    # extract the rows of the data frame which are excluded using the cls metric
+    limit_df = result_df[result_df['cls'] < 0.05]
+
+    # Construct plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot([0,1], [1,0], color = '0.75', linestyle = '--')
+    my_plot = plt.hexbin( x = limit_df['bre'],
+                          y = limit_df['brt'],
+                          C = limit_df['mass'],
+                          cmap = plt.cm.BuGn,
+                          reduce_C_function = np.max,
+                          gridsize = (25,5),
+                          vmin = 0.,
+                          vmax = 1050)
+    plt.axis([0, 1.1, 0, 1.1])
+    plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
+    plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
+    plt.grid(True)
+
+    # add color bar to right side
+    cb = plt.colorbar(my_plot,spacing='uniform',extend='max')
+    cb.set_label('Stop mass [GeV]')
+
+    # write plot to file
+    plt.savefig(out_file_name)
+
+
+# ------------------------------------------------------------------------------
+def plot_br_triangle_root(result_df, out_file_name):
+    """
+    Function takes a data frame, with branching ratios and CLs values.
+    Constructs a triangle with showing the CLs for each branching ratio choice
+    """
+    # get the x,y,z value
+    x_value , y_values, z_values = (result_df['bre'],
+                                    result_df['brt'],
+                                    result_df['cls'])
+
+    # the_graph =
+    result_df.plot(kind = 'hexbin',
+                   x = 'bre',
+                   y = 'brt',
+                   C = 'cls',
+                   cmap = plt.cm.BuGn_r,
+                   reduce_C_function = np.max,
+                   gridsize = (25,5),
+                   vmax = 0.25)
+    plt.axis([0, 1.1, 0, 1.1])
+    plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
+    plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
+    plt.savefig(out_file_name)
+
+
+# ------------------------------------------------------------------------------
+def plot_single_cls_plot(result_df, out_file_name):
+    """
+    Function takes a data frame, with masses and CLs values.
+    Constructs a plot of the CLs value vs mass.
+    """
+    # skip if data frame has no entries
+    if len(result_df) == 0: return
+
+    # extract mass and cls values from data frame
+    x_values = result_df['mass']
+    y_values = result_df['cls']
+
+    # plot the figure and save to file
+    plt.figure()
+    plt.xlabel('Stop mass [GeV]')
+    plt.ylabel('CL_{S}')
+    plt.plot(x_values, y_values)
+    plt.axhline(0.05, color = 'r', linestyle = '--')
+    plt.savefig(out_file_name, bbox_inches = 'tight')
+
+
+# ------------------------------------------------------------------------------
+def make_p_value_plots():
     # get results from hypothesis test
-    results = readHypoTestResults()
+    results = read_hypo_test_results()
 
-    # # convert results to x and y value lists
-    # x_vals = sorted(results.keys())
-    # y_vals = [results[x] for x in x_vals]
-    # print x_vals
-    # print y_vals
+    # make triangle plot for each stop mass
+    for mass in results['mass'].unique():
+        file_name = ''.join(('cls_vs_br',
+                             '_m_', str(mass),
+                             '.pdf'))
+        plot_cls_triangle(results[results['mass'] == mass], file_name)
 
-    # # make graph and set style options
-    # p_value_graph = ROOT.TGraph(len(x_vals), array.array('d', x_vals), array.array('d', y_vals))
-    # p_value_graph.SetMarkerStyle(20)
-    # p_value_graph.SetMarkerColor(ROOT.kBlue)
-    # p_value_graph.SetLineColor(ROOT.kBlue)
+    # make plot of mass limit in branching ratio triangle
+    plot_mass_limit_triangle(results, 'mass_limit.pdf')
 
-    # # get min/max for x axis
-    # x_min, x_max = getMinMax(x_vals)
-    # y_min, y_max = getMinMax(y_vals)
+    # make cls vs mass plot for each choice of branching ratios
+    for br_e, br_t, br_m in itertools.product(results['bre'].unique(),
+                                              results['brt'].unique(),
+                                              results['brm'].unique()):
+        file_name = ''.join(['cls_vs_m',
+                             '_br_e_', str(int(br_e*100)),
+                             '_br_m_', str(int(br_m*100)),
+                             '_br_t_', str(int(br_t*100)),
+                             '.pdf'])
+        plot_single_cls_plot(results[(results['bre'] == br_e) &
+                                     (results['brm'] == br_m) &
+                                     (results['brt'] == br_t)],
+                             file_name)
 
-    # print y_vals
-    # print 'ymin: ', y_min, ' ymax: ', y_max
-
-    # # make line to mark p = 0.05
-    # line = ROOT.TLine(x_min, 0.05, x_max, 0.05)
-    # line.SetLineColor(ROOT.kRed)
-    # line.SetLineWidth(4)
-    # line.SetLineStyle(ROOT.kDashed)
-
-    # # Create frame because TGraphs don't have axes
-    # frame = ROOT.TH1F('f', '', 1, x_min, x_max)
-    # frame.SetMinimum(y_min)
-    # frame.SetMaximum(y_max)
-    # frame.GetXaxis().SetTitle('m_{ #tilde{t}} [GeV]')
-    # frame.GetYaxis().SetTitle('CL_{s}')
-
-    # # Draw p value to plot
-    # c = ROOT.TCanvas('p_val')
-    # frame.Draw()
-    # p_value_graph.Draw('PL')
-    # line.Draw()
-
-    # c.Print('SamplePValue.pdf')
-
-# ------------------------------------------------------------------------------
-def main():
-    # input_file_name = 'SampleExcl_Output_hypotest__1_harvest_list'
-    # makePValuePlot(input_file_name)
-    makePValuePlot()
 
 # ==============================================================================
 if __name__ == "__main__":
-    main()
+    make_p_value_plots()
