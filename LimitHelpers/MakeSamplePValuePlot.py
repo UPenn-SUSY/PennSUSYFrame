0753dddee533ec9a9a02c4293707ad8060ab66c1
bdjackson <bjack3@gmail.com>
2014-11-26 11:59:09 +0100

    update expected limit plots

--- LimitHelpers/MakeSamplePValuePlot.py
+++ LimitHelpers/MakeSamplePValuePlot.py
@@ -10,6 +10,7 @@ import matplotlib as mpl
 mpl.rcParams['text.usetex']=True
 mpl.rcParams['text.latex.unicode']=True
 import ROOT
+import array
 
 import summary_harvest_tree_description as tree_summary
 
@@ -28,6 +29,13 @@ def extract_branching_ratios(file_name):
 def read_hypo_test_results():
     results = pandas.DataFrame(columns=('mass', 'bre', 'brm', 'brt', 'cls'))
 
+    # TEMPORARY: only keep a selection of points for now to keep a clean
+    # readable plot
+    br_points = [x/100. for x in xrange(0, 101, 25)]
+    allowed_points = [{'bre':e,'brm':m,'brt':(1-e-m)} for e,m in
+                      itertools.product(br_points, br_points) if e+m <= 1]
+    allowed_points.append({'bre':0.33, 'brm':0.33, 'brt':0.34})
+
     file_list = glob.glob('SampleExcl_*_harvest_list')
     for fl in file_list:
         branching_ratios = extract_branching_ratios(fl)
@@ -44,24 +52,10 @@ def read_hypo_test_results():
                              branching_ratios['brm'],
                              branching_ratios['brt'],
                              this_cls]
-            results.loc[results.shape[0]] = this_df_entry
+            if (branching_ratios in allowed_points):
+                results.loc[results.shape[0]] = this_df_entry
 
     return results.sort('mass')
-
-
-# ------------------------------------------------------------------------------
-def get_min_max(val_list, limit_min_to_zero=True):
-    min_val = min(val_list)
-    max_val = max(val_list)
-    delta = max_val - min_val
-
-    min_val -= 0.1 * delta
-    max_val += 0.1 * delta
-
-    if limit_min_to_zero:
-        min_val = max(0., min_val)
-
-    return (min_val, max_val)
 
 
 # ------------------------------------------------------------------------------
@@ -78,27 +72,30 @@ def plot_cls_triangle(result_df, out_file_name):
                           C = result_df['cls'],
                           cmap = plt.cm.BuGn_r,
                           reduce_C_function = np.max,
-                          gridsize = (25,5),
+                          gridsize = (20, 20),
                           vmin = 0.,
                           vmax = 0.10)
     plt.axis([0, 1.1, 0, 1.1])
     plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
     plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
     plt.grid(True)
+    plt.xticks([i*0.25 for i in xrange(5)])
+    plt.yticks([i*0.25 for i in xrange(5)])
 
     # add label with stop mass for this plot
     label_string = ''.join(('Stop mass = ',
                             str(result_df['mass'].iloc[0]),
                             ' GeV'))
     ax.text(0.65, 1.0, label_string,
-            bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
 
     # add color bar to right side
-    cb = plt.colorbar(my_plot,spacing='uniform',extend='max')
+    cb = plt.colorbar(my_plot, spacing = 'uniform', extend = 'max')
     cb.set_label('CL_{S}')
 
     # write plot to file
     plt.savefig(out_file_name)
+    plt.close()
 
 
 # ------------------------------------------------------------------------------
@@ -115,51 +112,55 @@ def plot_mass_limit_triangle(result_df, out_file_name):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plt.plot([0,1], [1,0], color = '0.75', linestyle = '--')
-    my_plot = plt.hexbin( x = limit_df['bre'],
-                          y = limit_df['brt'],
-                          C = limit_df['mass'],
-                          cmap = plt.cm.BuGn,
-                          reduce_C_function = np.max,
-                          gridsize = (25,5),
-                          vmin = 0.,
-                          vmax = 1050)
+    my_plot = plt.hexbin(x = limit_df['bre'],
+                         y = limit_df['brt'],
+                         C = limit_df['mass'],
+                         cmap = plt.cm.YlGnBu,
+                         reduce_C_function = np.max,
+                         gridsize = (20, 20),
+                         vmin = 0.,
+                         vmax = 1000)
     plt.axis([0, 1.1, 0, 1.1])
     plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
     plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
     plt.grid(True)
+    plt.xticks([i*0.25 for i in xrange(5)])
+    plt.yticks([i*0.25 for i in xrange(5)])
 
     # add color bar to right side
-    cb = plt.colorbar(my_plot,spacing='uniform',extend='max')
+    cb = plt.colorbar(my_plot, spacing = 'uniform')
     cb.set_label('Stop mass [GeV]')
 
     # write plot to file
     plt.savefig(out_file_name)
+    plt.close()
 
 
 # ------------------------------------------------------------------------------
-def plot_br_triangle_root(result_df, out_file_name):
+def plot_cls_triangle_root(result_df, out_file_name):
     """
     Function takes a data frame, with branching ratios and CLs values.
     Constructs a triangle with showing the CLs for each branching ratio choice
     """
     # get the x,y,z value
-    x_value , y_values, z_values = (result_df['bre'],
-                                    result_df['brt'],
-                                    result_df['cls'])
+    x_values , y_values, z_values = (result_df['bre'],
+                                     result_df['brt'],
+                                     result_df['cls'])
 
-    # the_graph =
-    result_df.plot(kind = 'hexbin',
-                   x = 'bre',
-                   y = 'brt',
-                   C = 'cls',
-                   cmap = plt.cm.BuGn_r,
-                   reduce_C_function = np.max,
-                   gridsize = (25,5),
-                   vmax = 0.25)
-    plt.axis([0, 1.1, 0, 1.1])
-    plt.xlabel('Br(\\tilde{t} \\rightarrow be)')
-    plt.ylabel('Br(\\tilde{t} \\rightarrow b\\tau)')
-    plt.savefig(out_file_name)
+    c = ROOT.TCanvas('c')
+    cls = ROOT.TH2F('cls',
+                    ';'.join(('',
+                              'Br(#tilde{t} #rightarrow be)',
+                              'Br(#tilde{t} #rightarrow b#tau)')),
+                    110, 0, 1.1,
+                    110, 0, 1.1)
+
+    for x, y, z in zip(x_values, y_values, z_values):
+        cls.Fill(x, y, z)
+
+    # Draw and print to file
+    cls.Draw('TEXT')
+    c.Print(out_file_name)
 
 
 # ------------------------------------------------------------------------------
@@ -175,13 +176,31 @@ def plot_single_cls_plot(result_df, out_file_name):
     x_values = result_df['mass']
     y_values = result_df['cls']
 
-    # plot the figure and save to file
-    plt.figure()
+    # plot the figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     plt.xlabel('Stop mass [GeV]')
     plt.ylabel('CL_{S}')
     plt.plot(x_values, y_values)
+
+    # add line at 0.05, where we place our limit
     plt.axhline(0.05, color = 'r', linestyle = '--')
+
+    # add label with stop mass for this plot
+    label_string = ''.join(('$Br(\\tilde{t} \\rightarrow be) = $',
+                            str(result_df['bre'].iloc[0]), '\n',
+                            '$Br(\\tilde{t} \\rightarrow b\\mu) = $',
+                            str(result_df['brm'].iloc[0]), '\n',
+                            '$Br(\\tilde{t} \\rightarrow b\\tau) = $',
+                            str(result_df['brt'].iloc[0])))
+    ax.text(0.1, 0.9, label_string,
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10},
+            verticalalignment = 'top',
+            transform=ax.transAxes)
+
+    # write plot to file
     plt.savefig(out_file_name, bbox_inches = 'tight')
+    plt.close()
 
 
 # ------------------------------------------------------------------------------
@@ -195,6 +214,13 @@ def make_p_value_plots():
                              '_m_', str(mass),
                              '.pdf'))
         plot_cls_triangle(results[results['mass'] == mass], file_name)
+
+    # make triangle plot for each stop mass
+    for mass in results['mass'].unique():
+        file_name = ''.join(('cls_vs_br_text',
+                             '_m_', str(mass),
+                             '.pdf'))
+        plot_cls_triangle_root(results[results['mass'] == mass], file_name)
 
     # make plot of mass limit in branching ratio triangle
     plot_mass_limit_triangle(results, 'mass_limit.pdf')
