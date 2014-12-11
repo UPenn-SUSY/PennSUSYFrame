a0884e86950fa14926737d179975acb4535e1f2a
bdjackson <bjack3@gmail.com>
2014-12-11 14:31:27 +0100

    update script to make contribution table

--- BMinusLAnalysis/ContributionTable/MakeContributionTable.py
+++ BMinusLAnalysis/ContributionTable/MakeContributionTable.py
@@ -16,6 +16,8 @@ laptop and run from there.
 import ROOT
 import pandas
 import sys
+import math
+import re
 
 # ------------------------------------------------------------------------------
 def extractRegionContributions(sample_file_name):
@@ -48,6 +50,34 @@ def extractRegionContributions(sample_file_name):
                     cont_df.loc[cont_df.shape[0]] = this_df_entry
     return cont_df
 
+# ______________________________________________________________________________
+def getNumString(number, num_digits):
+    """
+    Function to take a number and number of digits to display after
+    the decimal, and convert to a string of the correct length. If
+    the number is too small, return a string in scientific notation
+    """
+    if number == 0:
+        return '0'
+
+    cutoff = 10**(-num_digits)
+    if number >= cutoff:
+        return ''.join(['{:.', str(num_digits), 'f}']).format(number)
+    # do scientific notation
+    sci_number = ''.join(['{:.', str(num_digits), 'E}']).format(number)
+
+    # replace E notation with \cdot 10^... notation
+    sci_number = re.sub(r'E0+', 'E', sci_number)
+    sci_number = re.sub(r'E-0+', 'E-', sci_number)
+
+    # replace E notation with \cdot 10^... notation
+    sci_number = re.sub(r'E(.*)', r' \\cdot 10^{\1}', sci_number)
+
+    # add $'s around the number
+    sci_number = ''.join(['$', sci_number, '$'])
+
+    return sci_number
+
 # ------------------------------------------------------------------------------
 def createTableFromRegionContributions(region_df):
     # get region names
@@ -56,6 +86,8 @@ def createTableFromRegionContributions(region_df):
                                         | region_df['Region'].str.contains('_VR_')
                                         | region_df['Region'].str.contains('_SR')
                                         )
+                                      # remobe VR 1 for now
+                                      & ~region_df['Region'].str.contains('_VR_1')
                                       ].unique()
 
     # get sample names
@@ -80,6 +112,14 @@ def createTableFromRegionContributions(region_df):
     for region in region_names:
         this_region_string = region.replace('BMINUSL_', '')
         this_region_string = this_region_string.replace('_', ' ')
+        if this_region_string == 'CR TOP':
+            this_region_string = 'Top CR'
+        if this_region_string == 'CR Z':
+            this_region_string = 'Z CR'
+        if this_region_string == 'VR 3':
+            this_region_string = 'Top VR'
+        if this_region_string == 'VR 5':
+            this_region_string = 'Z VR'
         title_string += ' & %s' % this_region_string
     title_string += ' \\\\'
     print title_string
@@ -87,7 +127,49 @@ def createTableFromRegionContributions(region_df):
     # print individual bgk sample contributions
     print '\\midrule'
     for sample in bkg_sample_names:
-        sample_cont_string = sample
+        this_sample = sample
+        if this_sample == 'Z/#gamma^{*}':
+            this_sample = '$Z/\\gamma^{*}$'
+        if this_sample == 'ttbar':
+            this_sample = '$t\\bar{t}$'
+
+        sample_cont_string = [this_sample]
+
+        sample_subset = region_df[region_df.Sample == sample]
+        subset_regions = sample_subset.Region.unique()
+        for region in region_names:
+            this_value = 0
+            if region in subset_regions:
+                this_value = sample_subset[sample_subset.Region ==
+                                           region].iloc[0]['Count']
+            sample_cont_string.append(' & ')
+            sample_cont_string.append(getNumString(this_value, 1))
+            region_bkg_totals[region] += this_value
+        sample_cont_string.append(' \\\\')
+        # print sample_cont_string
+        print ''.join(sample_cont_string)
+
+    # print total background contributions
+    print '\\midrule'
+    # total_bkg_string = 'Total'
+    total_bkg_string = ['Total']
+    for region in region_names:
+        if region in subset_regions:
+            total_bkg_string.append(' & ')
+            total_bkg_string.append('\multirow{2}{*}{')
+            total_bkg_string.append(getNumString(region_bkg_totals[region], 1))
+            total_bkg_string.append('}')
+    # total_bkg_string += ' \\\\'
+    total_bkg_string.append(' \\\\')
+    # print total_bkg_string
+    print ''.join(total_bkg_string)
+    print 'background \\\\'
+
+    # print signal contributions
+    print '\\midrule'
+    for sample in sig_sample_names:
+        signal_cont_string = ['\multirow{2}{*}{', sample, '}']
+        signal_rel_cont_string = ['']
 
         sample_subset = region_df[region_df.Sample == sample]
         subset_regions = sample_subset.Region.unique()
@@ -95,35 +177,21 @@ def createTableFromRegionContributions(region_df):
             this_value = 0
             if region in subset_regions:
                 this_value = sample_subset[sample_subset.Region == region].iloc[0]['Count']
-            sample_cont_string += ' & %.1f' % this_value
-            region_bkg_totals[region] += this_value
-        sample_cont_string += ' \\\\'
-        print sample_cont_string
+            signal_cont_string.append(' & ')
+            signal_rel_cont_string.append(' & ')
 
-    # print total background contributions
-    print '\\midrule'
-    total_bkg_string = 'Total'
-    for region in region_names:
-        if region in subset_regions:
-            total_bkg_string += ' & \multirow{2}{*}{%.1f}' % region_bkg_totals[region]
-    total_bkg_string += ' \\\\'
-    print total_bkg_string
-    print 'background \\\\'
+            signal_cont_string.append(getNumString(this_value, 1))
 
-    # print signal contributions
-    print '\\midrule'
-    for sample in sig_sample_names:
-        sample_cont_string = sample
+            signal_rel_cont_string.append(' (')
+            signal_rel_cont_string.append(getNumString((this_value/region_bkg_totals[region]), 1))
+            signal_rel_cont_string.append(')')
 
-        sample_subset = region_df[region_df.Sample == sample]
-        subset_regions = sample_subset.Region.unique()
-        for region in region_names:
-            this_value = 0
-            if region in subset_regions:
-                this_value = sample_subset.Count[sample_subset.Region == region]
-            sample_cont_string += ' & %.1f (%.1f)' % (this_value, (this_value/region_bkg_totals[region]))
-        sample_cont_string += ' \\\\'
-        print sample_cont_string
+        signal_cont_string.append(' \\\\')
+        signal_rel_cont_string.append(' \\vspace{1ex} \\\\')
+
+        print ''.join(signal_cont_string)
+        print ''.join(signal_rel_cont_string)
+
     print '\\bottomrule'
 
     print '\\end{tabular}'
@@ -143,4 +211,7 @@ if __name__ == '__main__':
     # get input sample name
     test_sample_name = sys.argv[1] if len(sys.argv) > 1 else '${BASE_WORK_DIR}/compare_plots.b_minus_l.root'
     main(test_sample_name)
+
+    # for i in xrange(10):
+    #     print i, getNumString(3.1234567890, i), ' -- ', getNumString(0.00123456789, i)
 
