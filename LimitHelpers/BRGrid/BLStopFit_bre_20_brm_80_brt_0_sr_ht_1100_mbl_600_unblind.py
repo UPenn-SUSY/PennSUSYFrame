eca4737b2e3fddb8a44a0b8122686159d1ca57a2
bdjackson <bjack3@gmail.com>
2015-03-16 14:32:57 +0100

    up

--- LimitHelpers/BRGrid/BLStopFit_bre_20_brm_80_brt_0_sr_ht_1100_mbl_600_unblind.py
+++
@@ -1,7 +0,0 @@
-stop_br_e = 0.2
-stop_br_m = 0.8
-stop_br_t = 0.0
-test_sr = 'sr_ht_1100_mbl_600'
-is_blind = False
-
-execfile('/'.join([os.environ['BASE_WORK_DIR'], 'LimitHelpers', 'SampleExcl.py']))
