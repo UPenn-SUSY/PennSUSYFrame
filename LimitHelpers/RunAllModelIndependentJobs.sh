d4f1fd5cf34cc64b944fd07f8a8f97fded2a34bc
bdjackson <bjack3@gmail.com>
2015-05-08 16:58:05 +0200

    add model independent configs

---
+++ LimitHelpers/RunAllModelIndependentJobs.sh
@@ -0,0 +1,27 @@
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_400_all_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_400_all_unblind/Sig_excl_combined_NormalMeasurement_model.root -l 20.3 -o UpperLimit_table_sr_400_all.toys.tex
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_400_ee_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_400_ee_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_400_ee.toys.tex
+
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_400_em_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_400_mm_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_400_mm.toys.tex
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_400_mm_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_400_em_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_400_em.toys.tex
+
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_600_all_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_600_all_unblind/Sig_excl_combined_NormalMeasurement_model.root -l 20.3 -o UpperLimit_table_sr_600_all.toys.tex
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_600_ee_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_600_ee_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_600_ee.toys.tex
+
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_600_em_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_600_mm_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_600_mm.toys.tex
+
+HistFitter.py -p -t -w -f -F excl LimitHelpers/ModelIndependent/ModelIndependentExcl_sr_ht_1100_mbl_600_mm_unblind.py
+time UpperLimitTable.py -c SIG -w results/ModelIndependentExcl_sr_ht_1100_mbl_600_em_unblind/Sig_excl_combined_NormalMeasurement_model.root  -l 20.3 -o UpperLimit_table_sr_600_em.toys.tex
+
